7c06cb13abe7e12c8792460314ece4b7409b6a90
Igor Malyuta <igor.malyuta@customertimes.com>
2019-03-18 10:39:47 +0300

    sprint3 work

--- force-app/main/default/aura/PersonalSettings/PersonalSettingsController.js
+++ force-app/main/default/aura/PersonalSettings/PersonalSettingsController.js
@@ -3,12 +3,14 @@
         if(!communityService.isInitialized()) return;
         component.set('v.showSpinner', true);
         var tabId = communityService.getUrlParameter('tab');
-        if(tabId && ( (tabId === 'my-team' || tabId === 'delegates') || tabId === 'account-settings')) {
-            if(communityService.getUserMode() === 'Participant' && tabId === 'my-team'){
-                component.set('v.currentTab', 'delegates');
-            } else {
-                component.set('v.currentTab', tabId);
+        if(tabId) {
+            if(communityService.getUserMode() === 'HCP' || communityService.getUserMode() === 'PI') {
+                if (tabId === 'my-team' || tabId === 'account-settings')
+                    component.set('v.currentTab', tabId);
             }
+            else if(communityService.getUserMode() === 'Participant')
+                if(tabId === 'delegates' || tabId === 'account-settings')
+                    component.set('v.currentTab', tabId);
         }
         component.set('v.userMode', communityService.getUserMode());
         component.set('v.showSpinner', false);
