72436bbde0875c522fb75e8051aa025a956264df
Igor Malyuta <igor.malyuta@customertimes.com>
2019-03-18 15:50:51 +0300

    RRGoogleMap fix if BillingAddress empty

--- force-app/main/default/aura/RRGoogleMap/RRGoogleMapController.js
+++ force-app/main/default/aura/RRGoogleMap/RRGoogleMapController.js
@@ -2,37 +2,38 @@
  * Created by Igor Malyuta on 13.03.2019.
  */
 ({
-    doInit : function (component, event, helper) {
-        var markers = component.get('v.markers');
+    doInit: function (component, event, helper) {
+        var markers = [];
         var accounts = component.get('v.accounts');
-        for(var i = 0; i < accounts.length; i++) {
+        for (var i = 0; i < accounts.length; i++) {
             var clinicWrapper = accounts[i];
+            if(!clinicWrapper.clinic.BillingCity || !clinicWrapper.clinic.BillingLatitude)
+                continue;
 
             //Url forming
             var descriptionLink;
-            if(communityService.getUserMode() === 'PI') {
+            if (communityService.getUserMode() === 'PI') {
                 var url = window.location.href;
                 url = url.substring(0, url.lastIndexOf('/'));
                 var clinical = url + '/clinic-profile?id=' + clinicWrapper.clinic.Id;
 
-                descriptionLink = '<a href="' + clinical + '" target="_blank">Clinic profile</a>';
-            }
-            else
+                descriptionLink = '<a href=' + clinical + ' target=_blank>Clinic profile</a>';
+            } else
                 descriptionLink = '';
 
             markers.push({
                 location: {
-                    // Location Information
+                    Street: clinicWrapper.clinic.BillingStreet,
                     City: clinicWrapper.clinic.BillingCity,
-                    Country: clinicWrapper.clinic.BillingCountry,
                     PostalCode: clinicWrapper.clinic.BillingPostalCode,
                     State: clinicWrapper.clinic.BillingState,
-                    Street: clinicWrapper.clinic.BillingStreet,
+                    Country: clinicWrapper.clinic.BillingCountry,
+                    Latitude: clinicWrapper.clinic.BillingLatitude,
+                    Longitude: clinicWrapper.clinic.BillingLongitude
                 },
-                // Extra info for tile in sidebar & infoWindow
                 icon: 'custom:custom86',
-                title: clinicWrapper.name, // e.g. Account.Name
-                description : descriptionLink
+                title: clinicWrapper.name,
+                description: descriptionLink
             });
         }
 
