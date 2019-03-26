c4540c07c8f1e45fc923fbdf771d4c04667843a4
Andrii Kryvolap <andrii.kryvolap@customertimes.com>
2019-03-26 19:44:09 +0200

    REF_156 Distance logic for search for sites

--- force-app/main/default/aura/StudyListViewItem/StudyListViewItemController.js
+++ force-app/main/default/aura/StudyListViewItem/StudyListViewItemController.js
@@ -46,5 +46,11 @@
             cmp.set("v.detailsExpanded", true);
         }
 
+    },
+    navigateToSitesSearch : function (component, event, helper) {
+        var currentStudy = component.get('v.currentStudy');
+        var trial = currentStudy.trial;
+        var trialId = trial.Id;
+        communityService.navigateToPage("sites-search?id=" + trialId);
     }
 });
