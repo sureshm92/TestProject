8a3e61273dbaba8d74a8d80577e765448751cb8f
dmytro.pokutnii <dmytro.pokutnii@ctdev.io>
2019-03-28 17:45:22 +0200

    add trim for title and description

--- force-app/main/default/aura/Resource/ResourceController.js
+++ force-app/main/default/aura/Resource/ResourceController.js
@@ -2,29 +2,24 @@
 	{
 		doInit : function (component, event, helper) {
 
-			var spinner = component.find('spinner');
+			let spinner = component.find('spinner');
 			if(spinner){ spinner.show(); }
 
-			var resourceMode = component.get("v.resourceMode");
-			var resourceType = component.get("v.resourceType");
+			let resourceMode = component.get("v.resourceMode");
+			let resourceType = component.get("v.resourceType");
 
 			communityService.executeAction(component, 'getResources', {
 				resourceType: component.get('v.resourceType'),
 				resourceMode: component.get('v.resourceMode')
 			}, function (returnValue) {
 				if(!returnValue.errorMessage) {
-					for(let i = 0; i < returnValue.wrappers.length; i++) {
-						let title = returnValue.wrappers[i].title;
-						returnValue.wrappers[i].title = title.length > 55 ? title.substring(0, 52) + '...' : title;
-						let description = returnValue.wrappers[i].description;
-						returnValue.wrappers[i].description = description.length > 120 ? description.substring(0, 117) + '...' : description;
-					}
+					returnValue = helper.trimLongText();
 					component.set("v.resources", returnValue.wrappers);
 					component.set("v.errorMessage", "");
 				} else {
 					component.set("v.errorMessage", returnValue.errorMessage);
 				}
-				var spinner = component.find('spinner');
+				let spinner = component.find('spinner');
 				if(spinner){ spinner.hide(); }
 			});
 
--- force-app/main/default/aura/ResourceDashboardCardItem/ResourceDashboardCardItemController.js
+++ force-app/main/default/aura/ResourceDashboardCardItem/ResourceDashboardCardItemController.js
@@ -2,7 +2,7 @@
     {
         doInit : function (component, event, helper) {
 
-            var spinner = component.find('spinner');
+            let spinner = component.find('spinner');
             if(spinner){ spinner.show(); }
 
             communityService.executeAction(component, 'getResources', {
@@ -10,26 +10,21 @@
                 resourceMode: 'Default'
             }, function (returnValue) {
                 if(!returnValue.errorMessage) {
-                    for(let i = 0; i < returnValue.wrappers.length; i++) {
-                        let title = returnValue.wrappers[i].title;
-                        returnValue.wrappers[i].title = title.length > 40 ? title.substring(0, 37) + '...' : title;
-                        let description = returnValue.wrappers[i].description;
-                        returnValue.wrappers[i].description = description.length > 120 ? description.substring(0, 117) + '...' : description;
-                    }
+                    returnValue = helper.trimLongText(returnValue);
                     component.set("v.resources", returnValue.wrappers);
                     component.set("v.errorMessage", "");
                 } else {
                     component.set("v.errorMessage", returnValue.errorMessage);
                 }
-                var spinner = component.find('spinner');
+                let spinner = component.find('spinner');
                 if(spinner){ spinner.hide(); }
             });
         },
 
         navigateToPage : function (component, event, helper) {
-            var resourceType = event.currentTarget.dataset.type;
-            var resourceId = event.currentTarget.dataset.id;
-            var trialId = component.get('v.trialId');
+            let resourceType = event.currentTarget.dataset.type;
+            let resourceId = event.currentTarget.dataset.id;
+            let trialId = component.get('v.trialId');
             communityService.navigateToPage("resources?resourceType=" + resourceType + "&id=" + trialId + '&resId=' + resourceId);
         },
     }
