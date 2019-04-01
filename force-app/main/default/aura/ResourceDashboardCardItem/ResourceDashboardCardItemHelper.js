5f9c96d30cbd388fc56f43bc32bfb5e98b9135e8
dmytro.pokutnii <dmytro.pokutnii@ctdev.io>
2019-04-01 18:27:41 +0300

    fix description truncation on dashboard card

--- force-app/main/default/aura/ResourceDashboardCardItem/ResourceDashboardCardItemHelper.js
+++ force-app/main/default/aura/ResourceDashboardCardItem/ResourceDashboardCardItemHelper.js
@@ -6,7 +6,7 @@
                 let title = wrappers[i].title;
                 wrappers[i].title = title.length > 40 ? title.substring(0, 37) + '...' : title;
                 let description = wrappers[i].description;
-                wrappers[i].description = description.length > 120 ? description.substring(0, 117) + '...' : description;
+                wrappers[i].description = description.length > 83 ? description.substring(0, 80) + '...' : description;
             }
             return returnValue;
         }
