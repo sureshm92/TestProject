82a89bff7e99e550fc882abccaaf7237b69c90de
Denis <denis.zmiter@customertimes.com>
2019-04-02 13:02:05 +0300

    Updated Task Edit, Task table, Added custom labels

--- force-app/main/default/aura/TaskEdit/TaskEditController.js
+++ force-app/main/default/aura/TaskEdit/TaskEditController.js
@@ -3,60 +3,53 @@
  */
 ({
     doInit: function (component, event, helper) {
-        var todayDate = $A.localizationService.formatDate(new Date(), "YYYY-MM-DD");
+        var todayDate = $A.localizationService.formatDate(new Date(), 'YYYY-MM-DD');
         component.set('v.todayDate', todayDate);
         component.set('v.task', {
             sobjectType: 'Task'
         });
         const reminderFrequencyList = [
-            $A.get("$Label.c.One_day_before"),
-            $A.get("$Label.c.Complete_By_Date")
+            $A.get('$Label.c.One_day_before'),
+            $A.get('$Label.c.Complete_By_Date')
         ];
         component.set('v.reminderFrequencyList',reminderFrequencyList);
         const setReminderValueList = [
-            $A.get("$Label.c.Email"),
-            $A.get("$Label.c.Disabled")
+            $A.get('$Label.c.Email'),
+            $A.get('$Label.c.Disabled')
         ];
         component.set('v.setReminderValueList',setReminderValueList);
         var paramTaskId = communityService.getUrlParameter('id');
-        if (paramTaskId) {
-            component.set('v.editMode', true);
-
-            communityService.executeAction(component, 'getTaskById', {
-                "taskId": paramTaskId
-            }, function (task) {
-                component.set('v.task', task);
-                if (task.Status === 'Completed') {
-                    component.set('v.taskStatusCompleted', true);
-                }
-            }, null, function () {
-                component.find('spinner').hide();
-            });
-
-        } else {
-            component.set('v.editMode', false);
-            component.set('v.tascomponent.findk.Status', 'Open');
-            component.find('spinner').hide();
+        if(paramTaskId == undefined){
+            paramTaskId = null;
         }
-        if(!component.get('v.task.ActivityDate')){
-            var reminderFrequencyComponent = component.find("reminderFreqId");
-            reminderFrequencyComponent.set('v.disabled', true);
-        }
-
-        communityService.executeAction(component, 'checkEmailPreferencesIsOn', {}, function (on) {
-            component.set('v.emailTurnedOn', on);
-            if (on === false) {
-                var reminderDateComponent = component.find("reminderDateId");
-                var reminderFrequencyComponent = component.find("reminderFreqId");
+        communityService.executeAction(component, 'getTaskEditData', {
+            'taskId': paramTaskId
+        }, function (wrapper) {
+            component.set('v.task', wrapper.task);
+            if (wrapper.task.Status === 'Completed') {
+                component.set('v.taskStatusCompleted', true);
+            }
+            component.set('v.emailTurnedOn', wrapper.emailPreferencesIsOn);
+            if (wrapper.emailPreferencesIsOn === false) {
+                var reminderDateComponent = component.find('reminderDateId');
+                var reminderFrequencyComponent = component.find('reminderFreqId');
                 reminderFrequencyComponent.set('v.disabled', true);
                 reminderDateComponent.set('v.disabled', true);
             }
+            component.set('v.taskTypeList', wrapper.taskTypeList);
+            component.find('spinner').hide();
         });
-
-        communityService.executeAction(component, 'getTaskTypeList', {}, function (picklistValues) {
-            component.set('v.taskTypeList', picklistValues);
-        });
-
+        if (paramTaskId) {
+            component.set('v.editMode', true);
+        } else {
+            component.set('v.editMode', false);
+            component.set('v.tascomponent.findk.Status', 'Open');
+        }
+        if(!component.get('v.task.ActivityDate')){
+            var reminderFrequencyComponent = component.find('reminderFreqId');
+            reminderFrequencyComponent.set('v.disabled', true);
+            reminderFrequencyComponent.set('v.value', $A.get('$Label.c.Complete_By_Date'));
+        }
     },
 
     doCancel: function (component, event, helper) {
@@ -74,7 +67,7 @@
         communityService.executeAction(component, 'checkEmailPreferencesIsOn', {}, function () {
             component.find('spinner').show();
             communityService.executeAction(component, 'upsertTask', {
-                "paramTask": component.get('v.task')
+                'paramTask': component.get('v.task')
             }, function () {
                 window.history.go(-1);
             }, null, function () {
@@ -85,7 +78,14 @@
 
     doDeleteTask: function (component, event, helper) {
         communityService.executeAction(component, 'deleteTask', {
-            "paramTask": component.get('v.task')
+            'paramTask': component.get('v.task')
+        }, function (string) {
+            window.history.go(-1);
+        })
+    },
+    doDeleteTask: function (component, event, helper) {
+        communityService.executeAction(component, 'deleteTask', {
+            'paramTask': component.get('v.task')
         }, function (string) {
             window.history.go(-1);
         })
@@ -94,7 +94,7 @@
     doMarkAsCompleted: function (component, event, helper) {
         component.find('spinner').show();
         communityService.executeAction(component, 'markAsCompleted', {
-            "paramTask": component.get('v.task')
+            'paramTask': component.get('v.task')
         }, function (string) {
             window.history.go(-1);
         }, null, function () {
@@ -102,17 +102,17 @@
         })
     },
     onChangeFreq: function (component, event, helper) {
-        var freq = event.getSource().get("v.value");
-        var reminderDateComponent = component.find("reminderDateId");
+        var freq = event.getSource().get('v.value');
+        var reminderDateComponent = component.find('reminderDateId');
 
-        if (freq == $A.get("$Label.c.Complete_By_Date")) {
+        if (freq == $A.get('$Label.c.Complete_By_Date')) {
             reminderDateComponent.set('v.disabled', false);
-        } else if (freq == $A.get("$Label.c.One_day_before")) {
-            var reminderDateComponent = component.find("reminderDateId");
+        } else if (freq == $A.get('$Label.c.One_day_before')) {
+            var reminderDateComponent = component.find('reminderDateId');
             var dueDate = component.get('v.task.ActivityDate');
             var d = new Date(dueDate);
-            const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
-                "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+            const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
             ];
             var formatedDate = monthNames[d.getMonth()] + ' ' + (d.getDate() - 1) + ',' + d.getFullYear();
             reminderDateComponent.set('v.value', formatedDate);
@@ -120,35 +120,34 @@
         }
     },
     onChangeSetReminder: function (component, event, helper) {
-        var reminderFrequencyValue = event.getSource().get("v.value");
-        var reminderDateComponent = component.find("reminderDateId");
-        var reminderFrequencyComponent = component.find("reminderFreqId");
-        console.log(reminderFrequencyValue);
-        console.log($A.get("$Label.c.Disabled"));
-        if (reminderFrequencyValue == $A.get("$Label.c.Disabled")) {
+        var reminderFrequencyValue = event.getSource().get('v.value');
+        var reminderDateComponent = component.find('reminderDateId');
+        var reminderFrequencyComponent = component.find('reminderFreqId');
+        if (reminderFrequencyValue == $A.get('$Label.c.Disabled')) {
             reminderFrequencyComponent.set('v.disabled', true);
             reminderDateComponent.set('v.disabled', true);
             reminderDateComponent.set('v.value', '');
-        } else if (reminderFrequencyValue == $A.get("$Label.c.Email")) {
-            reminderFrequencyComponent.set('v.disabled', false);
+        } else if (reminderFrequencyValue == $A.get('$Label.c.Email')) {
+            if(component.get('v.task.ActivityDate')) {
+                reminderFrequencyComponent.set('v.disabled', false);
+            }
             reminderDateComponent.set('v.disabled', false);
         }
     },
     onChangeDueDate: function (component, event, helper) {
         var dueDate = component.get('v.task.ActivityDate');
-        var reminderFrequencyComponent = component.find("reminderFreqId");
-        var reminderDateComponent = component.find("reminderDateId");
-        if(component.get('v.emailTurnedOn') && component.find("reminderOptionsId").value() != $A.get("$Label.c.Disabled")){
-            if(component.get('v.task.ActivityDate')) {
-                var reminderFrequencyComponent = component.find("reminderFreqId");
+        var reminderFrequencyComponent = component.find('reminderFreqId');
+        var reminderDateComponent = component.find('reminderDateId');
+        if(component.get('v.emailTurnedOn') && component.find('reminderOptionsId').get('v.value') == $A.get('$Label.c.Email')){
+            if(dueDate) {
                 reminderFrequencyComponent.set('v.disabled', false);
             } else {
-                reminderFrequencyComponent.set('v.value', $A.get("$Label.c.Complete_By_Date"));
+                reminderFrequencyComponent.set('v.value', $A.get('$Label.c.Complete_By_Date'));
                 reminderFrequencyComponent.set('v.disabled', true);
                 reminderDateComponent.set('v.disabled', false);
             }
         }
-        if (reminderFrequencyComponent.get('v.value') == $A.get("$Label.c.Complete_By_Date")) {
+        if (reminderFrequencyComponent.get('v.value') == $A.get('$Label.c.Complete_By_Date')) {
             reminderDateComponent.set('v.value', dueDate);
         }
     }
