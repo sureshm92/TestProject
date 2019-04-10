6d1b9577bba284be16e27a6b5ed28d0a6ef8fe38
Yehor Dobrovolskyi <yehor.dobrovolskyi@ctdev.io>
2019-04-10 11:46:07 +0300

    PEH-180 (in progress) lookup modal.

--- force-app/main/default/aura/SearchConditionsOfInterest/SearchConditionsOfInterestController.js
+++ force-app/main/default/aura/SearchConditionsOfInterest/SearchConditionsOfInterestController.js
@@ -2,22 +2,37 @@
  * Created by Yehor Dobrovolskyi
  */
 ({
-    doInit: function(component, event, helper) {
+    doInit: function (component, event, helper) {
         component.find('searchModal').show();
     },
 
-    bulkSearch: function(component, event, helper) {
+    bulkSearch: function (component, event, helper) {
         var bypass = component.get('v.bypass');
-        if(bypass){
+        if (bypass) {
             return;
-        }else{
-            component.set('v.bypass',true);
+        } else {
+            component.set('v.bypass', true);
             window.setTimeout(
-                $A.getCallback(function() {
+                $A.getCallback(function () {
                     helper.valueChange(component, event, helper);
                 }), 500
             );
         }
+    },
+
+    handleChange: function (component, event) {
+        let taWrapper = event.getSource().get('v.value');
+        let taList = component.get('v.therapeuticAreas');
+        if (event.getParam('checked')) {
+            if (taList.length < 5) {
+                taList.push(taWrapper.therArea);
+            } else {
+                event.getSource().set('v.checked', false);
+            }
+        } else {
+            taList = taList.filter(e => e.Id !== taWrapper.therArea.Id);
+        }
+        component.set('v.therapeuticAreas', taList);
     },
 
 })
---
+++ force-app/main/default/aura/TestSearchApp/TestSearchApp.app
@@ -0,0 +1,6 @@
+<!--
+  - Created by Yehor Dobrovolskyi
+ -->
+<aura:application description="TestSearchApp" extends="force:slds">
+    <c:SearchConditionsOfInterest/>
+</aura:application>
