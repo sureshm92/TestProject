e512dc12e163dd70679906ecd4d3733b30b52f28
Igor Malyuta <igor.malyuta@customertimes.com>
2019-04-11 16:01:25 +0300

    StudyDetailView refactor

--- force-app/main/default/aura/MotivationalMessages/MotivationalMessagesController.js
+++ force-app/main/default/aura/MotivationalMessages/MotivationalMessagesController.js
@@ -4,11 +4,9 @@
 ({
     doExecute: function (component, event, helper) {
         var userMode =  communityService.getUserMode();
-        debugger;
         communityService.executeAction(component, 'getMotivationalMessage', {
             userMode: userMode
         }, function (message) {
-            debugger;
             if(message) communityService.showInfoToast('Info', message);
         })
     }
