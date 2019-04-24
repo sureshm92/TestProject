a8cc8e75c3f852ea8a9bb0a0680e4c9a5fd9566f
Denis Zmiter <denis.zmiter@customertimes.com>
2019-04-24 18:05:59 +0300

    Added new component to get and manage data from extarnall sources

--- force-app/main/default/aura/ExternalParticipant/ExternalParticipantController.js
+++ force-app/main/default/aura/ExternalParticipant/ExternalParticipantController.js
@@ -3,9 +3,26 @@
  */
 ({
     doInit: function (component, event, helper) {
+        if(component.get('v.participantId') == undefined) {
+            component.set('v.participantId', null)
+        }
         communityService.executeAction(component, 'getExternalParticipants', {
             'participantId': component.get('v.participantId')
         }, function (participants) {
+            console.log(participants[0]);
+            component.set('v.participants', participants);
+        });
+        communityService.executeAction(component, 'init', null, function (participant) {
+            component.set('v.participant', participant);
+        });
+    },
+
+    doSave: function (component, event, helper) {
+        console.log(JSON.stringify(component.get('v.participant')));
+        communityService.executeAction(component, 'saveParticipant', {
+            'participantJSON': JSON.stringify(component.get('v.participant'))
+        }, function (participants) {
+            console.log('here');
             component.set('v.participants', participants);
         });
     }
