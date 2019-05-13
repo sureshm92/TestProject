6d43f445a6a9af48f6c7b1c711b97b87c983d5b8
Yehor Dobrovolskyi <yehor.dobrovolskyi@ctdev.io>
2019-05-13 14:16:49 +0300

    PEH-609 refactoring. Add Platform Event on related objects.

--- force-app/main/default/aura/ResourceReminder/ResourceReminderController.js
+++ force-app/main/default/aura/ResourceReminder/ResourceReminderController.js
@@ -2,33 +2,11 @@
  * Created by Yehor Dobrovolskyi
  */({
     doInit: function (component, event, helper) {
-        let action = component.get('c.getReminders');
-        action.setParams({
-            resId: component.get('v.recordId')
-        });
-        action.setCallback(this, function(response) {
-            let state = response.getState();
-            if (state === "SUCCESS") {
-                console.log(response.getReturnValue());
-                component.set('v.reminders',response.getReturnValue());
-            }
-            else if (state === "ERROR") {
-                let errors = response.getError();
-                if (errors) {
-                    if (errors[0] && errors[0].message) {
-                        console.log("Error message: " +
-                            errors[0].message);
-                    }
-                } else {
-                    console.log("Unknown error");
-                }
-            }
-        });
-        $A.enqueueAction(action);
+        helper.subscribe(component, event);
+        // helper.remind(component, event);
     },
 
-    onLog: function (component, event, helper) {
-        console.log('test');
-
+    onRemind: function (component, event, helper) {
+        helper.remind(component, event);
     }
 })
