e85fd0f0f40ecdb62a053ca8931ab0b42d32fcbf
Dmytro Fedchyshyn <dmytro.fedchyshyn@ctdev.io>
2019-09-06 13:49:42 +0300

    Added validation on count of vandors

--- force-app/main/default/aura/VendorsTable/VendorsTableHelper.js
+++ force-app/main/default/aura/VendorsTable/VendorsTableHelper.js
@@ -5,13 +5,20 @@
 ({
     makeUnique: function (component, event) {
         let values = component.find('checkBoxBtn');
-        let value = event.getSource().get('v.value');
-        let isChecked = event.getSource().get('v.checked');
-        values.forEach(function (item) {
-            if (item.get('v.value') !== value && item.get("v.checked") === true) {
-                item.set("v.checked", false);
-            }
-        });
-        component.set("v.selectedVendor", isChecked ? value : '');
+        if (values.length > 1)
+        {
+            let value = event.getSource().get('v.value');
+            let isChecked = event.getSource().get('v.checked');
+            values.forEach(function (item) {
+                if (item.get('v.value') !== value && item.get("v.checked") === true) {
+                    item.set("v.checked", false);
+                }
+            });
+            component.set("v.selectedVendor", isChecked ? value : '');
+        } else {
+            let value = event.getSource().get('v.value');
+            let isChecked = event.getSource().get('v.checked');
+            component.set("v.selectedVendor", isChecked ? value : '');
+        }
     }
 });
