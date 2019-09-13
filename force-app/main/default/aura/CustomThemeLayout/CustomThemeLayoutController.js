e22bb69a655b113bc643ed41625e93b684b244b0
Leonid Bartenev <leonid.bartenev@customertimes.com>
2019-09-13 19:54:38 +0300

    Fixes

--- force-app/main/default/aura/CustomThemeLayout/CustomThemeLayoutController.js
+++ force-app/main/default/aura/CustomThemeLayout/CustomThemeLayoutController.js
@@ -7,7 +7,7 @@
         if(communityService.isInitialized()){
             component.set('v.allModes', communityService.getAllUserModes());
             component.set('v.currentMode', communityService.getCurrentCommunityMode());
-            component.find('spinner').hide();
+            //component.find('spinner').hide();
             component.set('v.isInitialized', true);
             component.set('v.showModeSwitcher', communityService.getAllUserModes().length > 1);
         }else{
