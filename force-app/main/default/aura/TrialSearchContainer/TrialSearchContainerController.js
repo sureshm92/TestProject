8efbbc3febc1bb295211401709f21c661c76a050
Yehor Dobrovolskyi <yehor.dobrovolskyi@ctdev.io>
2019-09-25 16:44:27 +0300

    PEH-49 - ref

--- force-app/main/default/aura/TrialSearchContainer/TrialSearchContainerController.js
+++ force-app/main/default/aura/TrialSearchContainer/TrialSearchContainerController.js
@@ -27,10 +27,7 @@
             String.format = function(format) {
                 var args = Array.prototype.slice.call(arguments, 1);
                 return format.replace(/{(\d+)}/g, function(match, number) {
-                    return typeof args[number] != 'undefined'
-                        ? args[number]
-                        : match
-                        ;
+                    return typeof args[number] != 'undefined' ? args[number] : match;
                 });
             };
         }
