2b63dfa8724d047d6a1c416684bfae64415c8073
Viacheslav Van <viacheslav.van@ctconsult.ru>
2019-10-30 13:58:32 +0300

    Translation File Tweaker

--- force-app/main/default/aura/XLF_PatternList/XLF_PatternListHelper.js
+++ force-app/main/default/aura/XLF_PatternList/XLF_PatternListHelper.js
@@ -3,24 +3,45 @@
  */
 
 ({
+        sortData: function (component, fieldName, sortDirection) {
+            var data = component.get("v.data");
+            var reverse = sortDirection !== 'asc';
+            data.sort(this.sortBy(fieldName, reverse))
+            component.set("v.data", data);
+        },
 
-    sortData: function (component, fieldName, sortDirection) {
-        var data = component.get("v.data");
-        var reverse = sortDirection !== 'asc';
-        data.sort(this.sortBy(fieldName, reverse))
-        component.set("v.data", data);
-    },
+        sortBy: function (field, reverse, primer) {
+            var key = primer ? function(x) {
+                    return primer(x[field])
+                } : function(x) {
+                    return x[field]
+                };
+            reverse = !reverse ? 1 : -1;
+            return function (a, b) {
+                return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
+            }
+        },
 
-    sortBy: function (field, reverse, primer) {
-        var key = primer ? function(x) {
-                return primer(x[field])
-            } : function(x) {
-                return x[field]
-            };
-        reverse = !reverse ? 1 : -1;
-        return function (a, b) {
-            return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
-        }
-    }
-
+//        startCheckDeploy : function(component, event, helper) {
+//            var checkerId = window.setInterval(function() { helper.checkDeploy(component, event, helper); }, 3000);
+//            component.set('v.checkerId', checkerId);
+//        },
+//
+//        stopCheckDeploy: function(component, event, helper) {
+//            window.clearInterval(component.get('v.checkerId'));
+//            component.set('v.checkerId', null);
+//        },
+//
+//        checkDeploy: function(component, event, helper) {
+//            helper.stopCheckDeploy(component, event, helper);
+//            var action = component.get("c.getDeployResult");
+//            action.setCallback (this, function(response) {
+//                if (response.getState() === "SUCCESS") {
+//                    console.log('      SUCCESS -------deploy status '+response.getReturnValue());
+//                } else if (response.getState() === "ERROR") {
+//                    console.log('      ERROR-------deploy status '+response.getReturnValue());
+//                }
+//            })
+//            $A.enqueueAction(action);
+//        }
 });
