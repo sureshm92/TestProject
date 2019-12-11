d0872693c0ea12e649b6a496a3e18ca64729c6fd
Yulia Yakushenkova <yuliya.yakushenkova@customertimes.com>
2019-12-11 19:13:30 +0300

    .

--- force-app/main/ui-community-template/lwc/lwcTestPage/lwcTestPage.js
+++ force-app/main/ui-community-template/lwc/lwcTestPage/lwcTestPage.js
@@ -2,9 +2,22 @@
  * Created by Igor Malyuta on 29.11.2019.
  */
 
-import {LightningElement} from 'lwc';
+import {LightningElement, track} from 'lwc';
 
 export default class LwcTestPage extends LightningElement {
+
+    currentPage = 1;
+    totalPages;
+    @track allRecordsCount = 20;
+
+    @track queryTerm;
+
+    handleKeyUp(evt) {
+        const isEnterKey = evt.keyCode === 13;
+        if (isEnterKey) {
+            this.queryTerm = evt.target.value;
+        }
+    }
 
     handleShow() {
         this.template.querySelector('c-web-popup').show();
@@ -16,5 +29,13 @@ export default class LwcTestPage extends LightningElement {
 
     doCancel() {
         this.template.querySelector('c-web-popup').hide();
+    }
+
+    changePagination(evt) {
+        this.allRecordsCount++;
+    }
+
+    doChange() {
+        console.log('Change Pagination' + this.allRecordsCount);
     }
 }
