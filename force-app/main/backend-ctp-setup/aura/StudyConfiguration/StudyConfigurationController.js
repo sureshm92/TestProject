a4bdd184635b5cf8483fd6a4680c2e511936bb87
Nargiz Mamedova <nargiz.mamedova@customertimes.com>
2019-12-13 12:33:37 +0300

    PEH-1219

--- force-app/main/backend-ctp-setup/aura/StudyConfiguration/StudyConfigurationController.js
+++ force-app/main/backend-ctp-setup/aura/StudyConfiguration/StudyConfigurationController.js
@@ -13,8 +13,17 @@
     },
 
     saveCTP: function (component, event, helper) {
-        let stValue = component.find('stToggle').get('v.checked');
-        component.find('vsToggle').set('v.checked', !stValue);
+        var source = event.getSource().get('v.name');
+        console.log('Source: ' + source);
+        var appEvent = $A.get("e.c:TaskToggleEvent");
+        if(source === 'visitScheduleToggle') {
+            let vsValue = component.find('vsToggle').get('v.checked');
+            component.find('stToggle').set('v.checked', !vsValue);
+        }
+        if(source === 'statusTimelineToggle') {
+            let stValue = component.find('stToggle').get('v.checked');
+            component.find('vsToggle').set('v.checked', !stValue);
+        }
 
         component.find('spinner').show();
         communityService.executeAction(component, 'saveChanges', {
@@ -22,6 +31,10 @@
         }, function () {
             component.find('spinner').hide();
             communityService.showSuccessToast('Success', 'Study Configuration setting saved!');
-        })
+        });
+        if(source === 'tasksToggle') {
+            $A.get('e.force:refreshView').fire();
+            appEvent.fire();
+        }
     }
 });
