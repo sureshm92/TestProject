65a670cb5b00eb70d17dae85eac8dc406e6258cb
Yulia Yakushenkova <yuliya.yakushenkova@customertimes.com>
2019-12-25 19:14:34 +0300

    PEH-1298

--- force-app/main/backend-ctp-setup/aura/VisitResultSharingCountryLookup/VisitResultSharingCountryLookupController.js
+++ force-app/main/backend-ctp-setup/aura/VisitResultSharingCountryLookup/VisitResultSharingCountryLookupController.js
@@ -10,7 +10,8 @@
             value: component.get('v.value'),
             includeStates: component.get('v.includeStates'),
             isInclude : component.get('v.isInclude'),
-            ctpId : component.get('v.ctpId')
+            ctpId : component.get('v.ctpId'),
+            countries: component.get('v.selectedCountries')
         }, function (searchResults) {
             params.callback(searchResults);
         })
@@ -21,6 +22,7 @@
         communityService.executeAction(component, 'searchResultsByValue', {
             value: component.get('v.value'),
             isInclude : component.get('v.isInclude'),
+            countries: component.get('v.selectedCountries')
         }, function (selection) {
             params.callback(selection);
         })
