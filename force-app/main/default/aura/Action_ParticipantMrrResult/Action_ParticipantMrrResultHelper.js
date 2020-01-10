d053795a4d6ba77c417b51297b4b46f5adb638f4
Jesterlolz <6976488@mail.ru>
2020-01-10 15:21:26 +0300

    REF-656

--- force-app/main/default/aura/Action_ParticipantMrrResult/Action_ParticipantMrrResultHelper.js
+++ force-app/main/default/aura/Action_ParticipantMrrResult/Action_ParticipantMrrResultHelper.js
@@ -1,11 +1,7 @@
-/**
- * Created by Leonid Bartenev
- */
 ({
-
     updateMRRStatus: function (component, status, gizmoData) {
-        component.set('v.showSpinner', true);
-        var pe = component.get('v.searchResult').pe;
+
+        var pe = component.get('v.pe');
         var action = component.get('c.setMRRStatus');
         action.setParams({
             peJSON: JSON.stringify(pe),
@@ -13,39 +9,32 @@
             surveyGizmoData: gizmoData
         });
         action.setCallback(this, function (response) {
-            var searchResult = component.get('v.searchResult');
-            if(searchResult.pe.Id) return;
             if (response.getState() === "SUCCESS") {
-                searchResult.pe = JSON.parse(response.getReturnValue());
-                component.set('v.searchResult', searchResult);
                 component.set("v.mrrResult", status);
+                component.get('v.callback')(pe);
             } else {
                 communityService.logErrorFromResponse(response);
                 communityService.showErrorToastFromResponse(response);
             }
-            component.set('v.showSpinner', false);
         });
         $A.enqueueAction(action);
     },
 
     addEventListener: function (component, helper) {
-        if(!component.serveyGizmoResultHandler){
-            component.serveyGizmoResultHandler = $A.getCallback(function(e) {
-                //window.removeEventListener('message', component.serveyGizmoResultHandler);
-                if(component.isValid()){
-                    if(e.data.messageType === 'SurveyGizmoResult'){
+        if (!component.serveyGizmoResultHandler) {
+            component.serveyGizmoResultHandler = $A.getCallback(function (e) {
+                if (component.isValid()) {
+                    if (e.data.messageType === 'SurveyGizmoResult') {
                         var gizmoData = null;
-                        if(e.data.pdfContent){
+                        if (e.data.pdfContent) {
                             gizmoData = e.data.pdfContent;
                         }
-                        if(e.data.success){
+                        if (e.data.success) {
                             helper.updateMRRStatus(component, 'Pass', gizmoData);
-                        }else{
+                        } else {
                             helper.updateMRRStatus(component, 'Fail', gizmoData);
                         }
-                        //console.log('Gizmo mrr result: ' + window.atob(e.data.pdfContent));
-                        //component.set('v.resultData', e.data.pdfContent);
-                    } else if(e.data.messageType === 'SurveyGizmoHeight'){
+                    } else if (e.data.messageType === 'SurveyGizmoHeight') {
                         component.set('v.frameHeight', (e.data.value + 30) + 'px');
                     }
                 }
@@ -53,5 +42,4 @@
             window.addEventListener('message', component.serveyGizmoResultHandler);
         }
     }
-
-})
+});
