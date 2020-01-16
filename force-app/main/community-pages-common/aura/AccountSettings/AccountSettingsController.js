4e8bbb260f55095eecb8e8b14ca2c1b2fb6602a1
Igor Malyuta <igor.malyuta@customertimes.com>
2020-01-16 14:07:06 +0300

    PEH-1400

--- force-app/main/community-pages-common/aura/AccountSettings/AccountSettingsController.js
+++ force-app/main/community-pages-common/aura/AccountSettings/AccountSettingsController.js
@@ -1,12 +1,12 @@
 ({
     doInit: function (component, event, helper) {
         component.find('spinner').show();
-        if(communityService.getCurrentCommunityMode().currentDelegateId) component.set('v.isDelegate', true);
+        if (communityService.getCurrentCommunityMode().currentDelegateId) component.set('v.isDelegate', true);
 
         communityService.executeAction(component, 'getInitData', {
             userMode: component.get('v.userMode')
         }, function (returnValue) {
-            var initData = JSON.parse(returnValue);
+            let initData = JSON.parse(returnValue);
             initData.password = {
                 old: '',
                 new: '',
@@ -20,115 +20,23 @@
 
             component.set('v.contact', initData.myContact);
             component.set('v.currentEmail', initData.myContact.Email);
-            // component.set('v.gendersLVList', initData.gendersLVList);
-            // component.set('v.statesByCountryMap', initData.statesByCountryMap);
-            // component.set('v.countriesLVList', initData.countriesLVList);
 
-            // if(initData.participant){
-            //     component.set('v.statesLVList', initData.statesByCountryMap[initData.participant.Mailing_Country_Code__c]);
-            //     helper.setParticipantSnapshot(component);
-            //     helper.setContactSnapshot(component);
-            // }else{
-            //     component.set('v.statesLVList', initData.statesByCountryMap[initData.myContact.MailingCountryCode]);
-            //     helper.setContactSnapshot(component);
-            // }
-            setTimeout($A.getCallback(function() {
-                helper.setFieldsValidity(component);
-                component.showHelpMessageIfInvalid();
-            }), 1000);
             component.set('v.isInitialized', true);
         }, null, function () {
             component.find('spinner').hide();
         })
     },
 
-    doShowHelpMessageIfInvalid: function(component){
-        var fieldsGroup = 'cField';
-        if(component.get('v.participant')) fieldsGroup = 'pField';
-        component.find(fieldsGroup).reduce(function (validSoFar, inputCmp) {
-            try{
-                inputCmp.showHelpMessageIfInvalid();
-            }catch (e) {
-                console.error(e);
-            }
-        }, true);
-    } ,
-
-    doUpdateParticipant: function (component, event, helper) {
-        component.find('spinner').show();
-        communityService.executeAction(component, 'updateParticipant', {
-            cont: JSON.stringify(component.get('v.contact')),
-            participantJSON : JSON.stringify(component.get('v.participant'))
-        }, function () {
-            component.set('v.participantHasUpdateTasks', false);
-            helper.setParticipantSnapshot(component);
-            var retString = communityService.getUrlParameter('ret');
-            if(retString) {
-                var retPage = communityService.getRetPage(retString);
-                communityService.navigateToPage(retPage);
-            }
-        }, null, function () {
-            component.find('spinner').hide();
-        });
-    },
-
-    doUpdateContact: function (component, event, helper) {
-        component.find('spinner').show();
-        communityService.executeAction(component, 'updateContact', {
-            cont: JSON.stringify(component.get('v.contact'))
-        }, function () {
-            component.set('v.participantHasUpdateTasks', false);
-            helper.setContactSnapshot(component);
-        }, null, function () {
-            component.find('spinner').hide();
-        });
-    },
-
-    doCountryCodeChanged: function(component, event, helper){
-        if(!component.get('v.isInitialized')) return;
-        var statesByCountryMap = component.get('v.statesByCountryMap');
-
-        if(component.get('v.participant')){
-            var participant = component.get('v.participant');
-            var states = statesByCountryMap[participant.Mailing_Country_Code__c];
-            component.set('v.statesLVList', states);
-            component.set('v.participant.Mailing_State_Code__c', null);
-        }else{
-            var contact = component.get('v.contact');
-            var states = statesByCountryMap[contact.MailingCountryCode];
-            component.set('v.statesLVList', states);
-            component.set('v.contact.MailingStateCode', null);
-        }
-    },
-
-    doParticipantChanged: function(component, event, helper){
-        if(!component.get('v.isInitialized')) return;
-        var snapShot = component.get('v.participantSnapshot');
-        var currentState = JSON.stringify(component.get('v.participant'));
-        component.set('v.participantChanged', snapShot !== currentState);
-    },
-
-    doContactChanged: function(component, event, helper){
-        if(!component.get('v.isInitialized')) return;
-        var snapShot = component.get('v.contactSnapshot');
-        var currentState = JSON.stringify(component.get('v.contact'));
-        component.set('v.contactChanged', snapShot !== currentState);
-    },
-
-    doCheckFieldsValidity: function(component, event, helper){
-        helper.setFieldsValidity(component);
-    },
-
     doChangeEmail: function (component, event, helper) {
-        var initData = component.get('v.initData');
-        var newEmail = initData.myContact.Email;
+        let initData = component.get('v.initData');
+        let newEmail = initData.myContact.Email;
         if (!newEmail) {
             communityService.showToast('error', 'error', $A.get('$Label.c.TST_Email_can_t_be_empty'));
             return;
         }
-        var oldEmail = component.get("v.currentEmail");
+        let oldEmail = component.get('v.currentEmail');
         if (newEmail === oldEmail) {
-            communityService.showToast("waring", "warning", $A.get('$Label.c.TST_Emails_are_same'));
+            communityService.showToast('waring', 'warning', $A.get('$Label.c.TST_Emails_are_same'));
             return;
         }
 
@@ -145,7 +53,7 @@
 
     doChangePassword: function (component, event, helper) {
         component.set('v.showSpinner', true);
-        var initData = component.get('v.initData');
+        let initData = component.get('v.initData');
 
         communityService.executeAction(component, 'changePassword', {
             newPassword: initData.password.new,
@@ -153,7 +61,7 @@
             oldPassword: initData.password.old
         }, function (returnValue) {
             communityService.showToast('success', 'success', $A.get('$Label.c.TST_Your_password_has_been_changed_successfully'));
-            var initData = component.get('v.initData');
+            let initData = component.get('v.initData');
             component.set('v.initData.password', {
                 old: '',
                 new: '',
@@ -165,7 +73,7 @@
     },
 
     doSwitchOptInEmail: function (component, event, helper) {
-        var initData = component.get('v.initData');
+        let initData = component.get('v.initData');
         communityService.executeAction(component, 'changeOptInEmail', {
             participantOptInStatusEmail: initData.myContact.Participant_Opt_In_Status_Emails__c,
             hcpOptInPatientEmail: initData.myContact.HCP_Opt_In_Patient_Status_Emails__c,
@@ -177,11 +85,16 @@
     },
 
     doSwitchOptInSMS: function (component, event, helper) {
-        helper.changeSMSOption(component);
+        let personWrapper = component.get('v.personWrapper');
+        communityService.executeAction(component, 'changeOptInSMS', {
+            participantOptInStatusSMS: personWrapper.optInSMS
+        }, function () {
+            component.find('contact-info-section').scrollOnMobileField(personWrapper.optInSMS);
+        });
     },
 
     doSwitchOptInCookies: function (component, event, helper) {
-        var initData = component.get('v.initData');
+        let initData = component.get('v.initData');
         communityService.executeAction(component, 'changeOptInCookies', {
             rrCookieAllowed: initData.myContact.RRCookiesAllowedCookie__c,
             rrLanguageAllowed: initData.myContact.RRLanguageAllowedCookie__c
@@ -191,7 +104,7 @@
     },
 
     doSubmitQuestion: function (component, event, helper) {
-        var description = component.get('v.privacyFormText');
+        let description = component.get('v.privacyFormText');
         if (!description) {
             communityService.showToast('warning', 'warning', $A.get('$Label.c.TST_Complete_Your_Question'));
             return;
@@ -208,8 +121,8 @@
             component.set('v.showSpinner', false);
         })
     },
-    
-    checkUpdate : function (component, event, helper) {
+
+    checkUpdate: function (component, event, helper) {
         component.set('v.isUpdated', true);
     },
 
@@ -218,27 +131,31 @@
             contactId: component.get('v.initData.myContact.Id'),
             isOptIn: component.get('v.initData.myContact.Visit_Results_Opt_In__c')
         }, function (returnValue) {
-            component.set("v.initData.myContact.Visit_Results_Opt_In__c", returnValue);
-            component.set("v.showModal", false);
+            component.set('v.initData.myContact.Visit_Results_Opt_In__c', returnValue);
+            component.set('v.showModal', false);
         });
     },
 
     handleCancelClick: function (component, event, helper) {
-        component.set("v.initData.myContact.Visit_Results_Opt_In__c", false);
-        component.set("v.showModal", false);
-
+        component.set('v.initData.myContact.Visit_Results_Opt_In__c', false);
+        component.set('v.showModal', false);
     },
 
     openVisitResultsOptInModal: function (component, event, helper) {
-        if (component.get("v.initData.myContact.Visit_Results_Opt_In__c")) {
-            component.set("v.showModal", true);
+        if (component.get('v.initData.myContact.Visit_Results_Opt_In__c')) {
+            component.set('v.showModal', true);
         } else {
             communityService.executeAction(component, 'changeOptInVisitResults', {
-                     contactId: component.get('v.initData.myContact.Id'),
-                     isOptIn: component.get('v.initData.myContact.Visit_Results_Opt_In__c')
-                 }, function (returnValue) {
-                component.set("v.initData.myContact.Visit_Results_Opt_In__c", returnValue);
+                contactId: component.get('v.initData.myContact.Id'),
+                isOptIn: component.get('v.initData.myContact.Visit_Results_Opt_In__c')
+            }, function (returnValue) {
+                component.set('v.initData.myContact.Visit_Results_Opt_In__c', returnValue);
             });
         }
     },
-})
+
+    onEditPerson: function (component, event, helper) {
+        let personWrapper = event.getSource().get('v.personWrapper');
+        component.set('v.personWrapper', personWrapper);
+    }
+});
