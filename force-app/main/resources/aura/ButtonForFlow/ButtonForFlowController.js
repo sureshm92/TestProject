fc8eafb4f809696e35f9cb383b392473066a1364
OSkrynnikova <olga.skrynnikova@customertimes.com>
2020-01-20 16:24:24 +0300

    PEH-1407 fix

--- force-app/main/resources/aura/ButtonForFlow/ButtonForFlowController.js
+++ force-app/main/resources/aura/ButtonForFlow/ButtonForFlowController.js
@@ -7,7 +7,30 @@
         component.set('v.isOpen', true);
         var flow = component.find('flow');
         let recordId = component.get('v.recordId');
-        flow.startFlow('Study_Resources', recordId);
+        var pageRef = component.get("v.pageReference");
+        if(pageRef != null) {
+            var state = pageRef.state;
+            if(state != null) {
+                var base64Context = state.inContextOfRef;
+                if (base64Context) {
+                    if (base64Context.startsWith("1\.")) {
+                        base64Context = base64Context.substring(2);
+                        var addressableContext = window.atob(base64Context);
+                        var pageReferencePrevious = JSON.parse(addressableContext);
+            			if(pageReferencePrevious != null && pageReferencePrevious.attributes != null){
+                            if(pageReferencePrevious.attributes.objectApiName == 'Clinical_Trial_Profile__c') {
+                    			recordId = pageReferencePrevious.attributes.recordId;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        console.log(recordId);
+        let inputVars = [
+                { name: "recordId", type: "String", value: recordId}     
+        ];
+        flow.startFlow('Study_Resources', inputVars);
     },
 
     closeFlowModal : function(component, event, helper) {
