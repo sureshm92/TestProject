15feb783d610e3e126049a0119250724c4ff06fa
Leonid Bartenev <leonid.bartenev@customertimes.com>
2020-02-03 15:02:48 +0300

    .

--- force-app/main/default/aura/PSECard/PSECardController.js
+++ force-app/main/default/aura/PSECard/PSECardController.js
@@ -7,7 +7,7 @@
     doInit: function (component, event, helper) {
         if (component.get('v.pe')) {
             let pe = component.get('v.pe');
-            if (pe.Participant__r && pe.Participant__r.Adult__c) {
+            if (pe.Participant__r && pe.Participant__r.Adult__c && communityService.getUserMode() !== 'Participant') {
                 component.set('v.parent.parent.hasEmancipatedParticipants', pe.Participant__r.Adult__c);
             }
         }
