152837e62a2af60bb891adeeefe096f889100d18
Aleksey Moseev <alexey.moseev@customertimes.com>
2020-02-04 11:57:15 +0300

    REF-739 Notification about a Participant emancipation
    REF-739 Notification about a Participant emancipation

--- force-app/main/default/aura/ParticipantCardIsEmansipated/ParticipantCardIsEmansipatedController.js
+++ force-app/main/default/aura/ParticipantCardIsEmansipated/ParticipantCardIsEmansipatedController.js
@@ -7,9 +7,9 @@
     doInit: function (component, event, helper) {
         let participant = component.get('v.pe.Participant__r');
         if (participant && participant.Date_of_Birth__c && participant.Emancipation_in_progress__c) {
-            let birthDate = new Date(participant.Date_of_Birth__c);
-            let todayDate = new Date();
-            component.set('v.needEmancipationProcess', birthDate >= todayDate);
+            //let birthDate = new Date(participant.Date_of_Birth__c);
+            //let todayDate = new Date();
+            component.set('v.needEmancipationProcess', participant.Emancipation_in_progress__c);
         }
     }
 
