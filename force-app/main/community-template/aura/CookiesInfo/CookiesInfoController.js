0f52d364ff8308fff4ee1cc4abb0e076f63030d5
Igor Malyuta <igor.malyuta@customertimes.com>
2020-04-22 14:47:06 +0300

    .

--- force-app/main/community-template/aura/CookiesInfo/CookiesInfoController.js
+++ force-app/main/community-template/aura/CookiesInfo/CookiesInfoController.js
@@ -2,15 +2,15 @@
  * Created by Leonid Bartenev
  */
 ({
-    doInit: function (component, event, hepler) {
-        var rrCookies = communityService.getCookie('RRCookies');
+    doInit: function (component, event, helper) {
+        let rrCookies = communityService.getCookie('RRCookies');
         if(!rrCookies){
-            var infoText = $A.get('$Label.c.Cookies_Info_Text');
-            var linkCookies = $A.get('$Label.c.Link_Cookies');
-            var urlCookies = $A.get('$Label.c.URL_Cookies');
-            var linkPP = $A.get('$Label.c.Footer_Link_Privacy_Policy');
-            var linkIAB = $A.get('$Label.c.Link_Interactive_Advertising_Bureau');
-            var urlIAB = $A.get('$Label.c.URL_Interactive_Advertising_Bureau')
+            let infoText = $A.get('$Label.c.Cookies_Info_Text');
+            let linkCookies = $A.get('$Label.c.Link_Cookies');
+
+            let linkPP = $A.get('$Label.c.Footer_Link_Privacy_Policy');
+            let linkIAB = $A.get('$Label.c.Link_Interactive_Advertising_Bureau');
+            let urlIAB = $A.get('$Label.c.URL_Interactive_Advertising_Bureau')
             infoText = infoText.replace('##cookiesURL', '<a class="ci-link" href="/s/cookie-policy">' + linkCookies + '</a>');
             infoText = infoText.replace('##privacyPolicyURL', '<a class="ci-link" href="/s/privacy-policy">' + linkPP +'</a>');
             infoText = infoText.replace('##interactiveAdvertisingBureauURL', '<a class="ci-link" href="' + urlIAB +'">' + linkIAB + '</a>');
@@ -23,16 +23,16 @@
             });
             document.body.addEventListener('click', component. cookiesOff, false);
             setTimeout($A.getCallback(function() {
-                this.cookiesOff;
+                component .cookiesOff();
             }), 10000);
         }
+
     },
 
-    doCloseCookiesInfo: function (component, event, helper) {
+    doCloseCookiesInfo: function (component) {
         communityService.setCookie('RRCookies', 'agreed');
-        var appEvent = $A.get("e.c:TCCookieClassesRemove");
+        let appEvent = $A.get("e.c:TCCookieClassesRemove");
         appEvent.fire();
         component.set('v.visible', false);
-    },
-
+    }
 })
