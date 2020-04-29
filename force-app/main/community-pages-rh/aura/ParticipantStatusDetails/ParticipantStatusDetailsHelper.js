2d49f8a45138f8256542bcbaab7b2024fb56fa6e
AndriiKryvolap <andrii.kryvolap@customertimes.com>
2020-04-29 22:08:44 +0300

    REF-1163.

--- force-app/main/community-pages-rh/aura/ParticipantStatusDetails/ParticipantStatusDetailsHelper.js
+++ force-app/main/community-pages-rh/aura/ParticipantStatusDetails/ParticipantStatusDetailsHelper.js
@@ -34,9 +34,9 @@
                 for (let j = 0; j < stepWrapper.formFieldGroups[i].fields.length; j++) {
                     for (let k = 0; k < dependentFields.length; k++) {
                         if (dependentFields[k].fieldName === stepWrapper.formFieldGroups[i].fields[j].field) {
-                            if (dependentFields[k].controllingValue.indexOf(value) !== -1 || value === '') {
+                            if (dependentFields[k].controllingValue.indexOf(value) !== -1 && value !== '') {
                                 stepWrapper.formFieldGroups[i].fields[j].dependentActive = true;
-                                if (stepWrapper.formFieldGroups[i].fields[j].populateFromDependent !== null && (stepWrapper.formFieldGroups[i].fields[j].value === null && stepWrapper.formFieldGroups[i].fields[j].value !== '')) {
+                                if (stepWrapper.formFieldGroups[i].fields[j].populateFromDependent !== null && (stepWrapper.formFieldGroups[i].fields[j].value === null && stepWrapper.formFieldGroups[i].fields[j].value === '')) {
                                     for (let k = 0; k < stepWrapper.formFieldGroups.length; k++) {
                                         for (let l = 0; l < stepWrapper.formFieldGroups[k].fields.length; l++) {
                                             if (stepWrapper.formFieldGroups[i].fields[j].populateFromDependent === stepWrapper.formFieldGroups[k].fields[l].field)
