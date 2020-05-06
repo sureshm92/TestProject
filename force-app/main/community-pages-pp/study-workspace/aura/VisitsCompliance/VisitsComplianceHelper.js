5709f9e07eb2a73e432ebe6ed7d96e3de0ab7f34
Dmitrii Ivakhnenko <dmitrii.ivakhnenko@customertimes.com>
2020-05-06 15:10:38 +0300

    PEH-1958 PEH-1969 PEH-1984

--- force-app/main/community-pages-pp/study-workspace/aura/VisitsCompliance/VisitsComplianceHelper.js
+++ force-app/main/community-pages-pp/study-workspace/aura/VisitsCompliance/VisitsComplianceHelper.js
@@ -2,17 +2,4 @@
  * Created by dmytro.fedchyshyn on 08.07.2019.
  */
 
-({
-    toggleOptIn: function (component, event) {
-
-        if (communityService.isInitialized()) {
-            communityService.executeAction(component, 'changeOptInOnTrue', {
-                    contactId: component.get("v.contactId")
-                }, function (returnValue) {
-                component.set("v.isOptIn", returnValue);
-                component.set('v.optInChanged', true);
-                component.set("v.showModal", false);
-            });
-        }
-    },
-});
+({});
