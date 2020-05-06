4bae9bea7b7cf3d75bd76be5dc7194b2b9f9c956
AndriiKryvolap <andrii.kryvolap@customertimes.com>
2020-05-06 14:58:58 +0300

    REF-1253.Counter.

---
+++ force-app/main/incentive-program/lwc/navIncentiveDropDown/navIncentiveDropDown.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Andrii Kryvolap.
+ */
+
+import {LightningElement, api} from 'lwc';
+
+export default class NavIncentiveDropDown extends LightningElement {
+
+    @api totalPoints;
+    @api lastPoints;
+
+}
