b45062a407da3c2633bbefbb9adbca9e67aac1a6
Igor Iosifov <igor.iosifov@customertimes.com>
2020-05-18 23:55:54 +0300

    temp

--- force-app/main/community-pages-pp/study-workspace/aura/VisitResultsPanel/VisitResultsPanelController.js
+++ force-app/main/community-pages-pp/study-workspace/aura/VisitResultsPanel/VisitResultsPanelController.js
@@ -4,12 +4,18 @@
 
             component.set('v.initialized', false);
             component.set('v.displayPanel', true);
+            let asd = component.get('v.visitResultGroupName');
+            let dsa = component.get('v.visitResultsShares');
+            debugger
             let spinner = component.find('mainSpinner');
             if(spinner) {
                 spinner.show();
             }
+            let sad = component.get('v.visitResultsShares');
+            debugger
             communityService.executeAction(component, 'getVisitResultsContainer', {
-                    visitResultGroupName: component.get('v.visitResultGroupName')
+                    visitResultGroupName: component.get('v.visitResultGroupName'),
+                    visitResultsShares: component.get('v.visitResultsShares')
                 }, function (returnValue) {
                     if(returnValue === null) {
                         component.set('v.displayPanel', false);
