b3b3be96e0400583f1b0adfb8bd7fda6223b9063
Igor Iosifov <igor.iosifov@customertimes.com>
2020-05-19 13:18:27 +0300

    PEH-2051

--- force-app/main/community-pages-pp/study-workspace/aura/VisitResultsPanel/VisitResultsPanelController.js
+++ force-app/main/community-pages-pp/study-workspace/aura/VisitResultsPanel/VisitResultsPanelController.js
@@ -4,18 +4,14 @@
 
             component.set('v.initialized', false);
             component.set('v.displayPanel', true);
-            let asd = component.get('v.visitResultGroupName');
-            let dsa = component.get('v.visitResultsShares');
-            debugger
             let spinner = component.find('mainSpinner');
             if(spinner) {
                 spinner.show();
             }
-            let sad = component.get('v.visitResultsShares');
-            debugger
+            let visitResultsShares = JSON.stringify(component.get('v.initData.visitResultsShares'));
             communityService.executeAction(component, 'getVisitResultsContainer', {
                     visitResultGroupName: component.get('v.visitResultGroupName'),
-                    visitResultsShares: component.get('v.visitResultsShares')
+                    visitResultsShares: visitResultsShares
                 }, function (returnValue) {
                     if(returnValue === null) {
                         component.set('v.displayPanel', false);
@@ -31,4 +27,4 @@
             );
         },
     }
-)
+);
