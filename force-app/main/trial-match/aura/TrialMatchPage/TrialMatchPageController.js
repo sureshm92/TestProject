4eed020a128e40597b778ffa96fb0aa72401787c
Igor Malyuta <igor.malyuta@customertimes.com>
2020-05-22 16:28:32 +0300

    PEH-1850

--- force-app/main/trial-match/aura/TrialMatchPage/TrialMatchPageController.js
+++ force-app/main/trial-match/aura/TrialMatchPage/TrialMatchPageController.js
@@ -11,6 +11,7 @@
             communityService.executeAction(component, 'getMatchCTPs', null, function (data) {
                 component.set('v.trialmatchCTPs', data.trialmatchctps);
                 component.set('v.initializedTrms', true);
+                component.set('v.initialized', true);
 
                 if (!String.format) {
                     String.format = function (format) {
