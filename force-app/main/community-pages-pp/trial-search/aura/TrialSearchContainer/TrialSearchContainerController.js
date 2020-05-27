91c5238ad39696190349b3ba47776eed5c1ab30d
Igor Malyuta <igor.malyuta@customertimes.com>
2020-05-27 14:38:51 +0300

    PEH-428

--- force-app/main/community-pages-pp/trial-search/aura/TrialSearchContainer/TrialSearchContainerController.js
+++ force-app/main/community-pages-pp/trial-search/aura/TrialSearchContainer/TrialSearchContainerController.js
@@ -7,35 +7,26 @@
 
         if(!communityService.isDummy()) {
             component.find('mainSpinner').show();
-            helper.enqueue(component, 'c.getSearchCTPs', {})
-                .then(function (data) {
-                    console.log('data ', data);
-                    if (!data.isAllowed) communityService.navigateToHome();
-                    component.set('v.trialTDOs', data.trialTDOs);
-                    component.set('v.taps', data.taps);
-                    component.set('v.participant', data.participant);
-                    component.set('v.formData', data.formData);
-                    component.set('v.isInitialized', true);
-                    component.set('v.initializedTaps', true);
-                    component.find('mainSpinner').hide();
-                }, function (err) {
-                    if (err && err[0].message) {
-                        component.set('v.errorMessage', err[0].message);
-                    }
-                    console.log('error:', err[0].message);
-                    component.find('mainSpinner').hide();
-                }).catch(function (err) {
-                console.error(err);
+            communityService.executeAction(component, 'getSearchCTPs', null, function (data) {
+                if (!data.isAllowed) communityService.navigateToHome();
+                component.set('v.trialTDOs', data.trialTDOs);
+                component.set('v.taps', data.taps);
+                component.set('v.participant', data.participant);
+                component.set('v.formData', data.formData);
+                component.set('v.isInitialized', true);
+                component.set('v.initializedTaps', true);
+
+                if (!String.format) {
+                    String.format = function (format) {
+                        var args = Array.prototype.slice.call(arguments, 1);
+                        return format.replace(/{(\d+)}/g, function (match, number) {
+                            return typeof args[number] != 'undefined' ? args[number] : match;
+                        });
+                    };
+                }
+            }, null, function () {
                 component.find('mainSpinner').hide();
             });
-            if (!String.format) {
-                String.format = function (format) {
-                    var args = Array.prototype.slice.call(arguments, 1);
-                    return format.replace(/{(\d+)}/g, function (match, number) {
-                        return typeof args[number] != 'undefined' ? args[number] : match;
-                    });
-                };
-            }
         } else {
             component.find('builderStub').setPageName(component.getName());
         }
