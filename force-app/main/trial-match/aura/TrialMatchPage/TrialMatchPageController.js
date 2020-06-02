7a27ced940b114164bf5c579ed83c892f883b8b2
Sravani <sravani.dasari@quintiles.com>
2020-06-02 18:06:52 +0530

    UI Defect fixes PEH-2223

--- force-app/main/trial-match/aura/TrialMatchPage/TrialMatchPageController.js
+++ force-app/main/trial-match/aura/TrialMatchPage/TrialMatchPageController.js
@@ -10,6 +10,7 @@
 
             communityService.executeAction(component, 'getMatchCTPs', null, function (data) {
                 component.set('v.trialmatchCTPs', data.trialmatchctps);
+                component.set('v.peid', data.trialmatchctps.participantid);
                 component.set('v.initialized', true);
 
                 if (!String.format) {
@@ -26,5 +27,10 @@
         } else {
             component.find('builderStub').setPageName(component.getName());
         }
+    },
+
+    doGenerateReport: function (component, event, helper) {
+        var peid = component.get('v.peid');
+        window.open('trial-match-print-view?id=' + peid, '_blank');
     }
 });
