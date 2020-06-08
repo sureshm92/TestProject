21341e58843fd6e2af7d3ca9c78a6a6b5f6f0007
Bijesh <bijesh.kumar@quintiles.com>
2020-06-08 17:41:40 +0530

    PEH-1515

--- force-app/main/default/aura/LanguageSwitcher/LanguageSwitcherController.js
+++ force-app/main/default/aura/LanguageSwitcher/LanguageSwitcherController.js
@@ -3,24 +3,12 @@
  */
 ({
     doInit: function (component, event, helper) {
-       
-        component.set('v.userMode', communityService.getUserMode());
-        communityService.executeAction(component, 'getInitData',  null, function (returnValue) {
-            let initData = JSON.parse(returnValue);
-			
-            
+        communityService.executeAction(component, 'getInitData', null, function (returnValue) {
+            var initData = JSON.parse(returnValue);
             component.set('v.languages', initData.languages);
-			component.set('v.countries', initData.countries);
-			component.set('v.statesByCountryMap', initData.statesByCountryMap);
             component.set('v.optionalLanguages', initData.optionalLanguages);
             component.set('v.locales',   initData.locales);
             component.set('v.timezones', initData.timezones);
-           
-
-            let personWrapper = component.get('v.personWrapper');                        
-            //helper.setPersonSnapshot(component);
-           
-   
             console.log(initData.languages);
             setTimeout($A.getCallback(function () {
                 component.set('v.languageKey',   initData.languageKey);
@@ -35,55 +23,23 @@
     },
 
     doChangeLanguage: function(component){
-		
         if(!component.get('v.initialized')) return;
-        var languageKey   = component.get('v.languageKey');		
-		var countryName, stateName, zipcode;
-		//Sneha-Logic to fetch the Country Name through country code and pass it to the controller
-		if(component.get('v.userMode') == 'Participant'){
-			
-			var tempcountries = component.get("v.countries"),		
-			value = component.find("countryList").get("v.value"),		
-			index = tempcountries.findIndex(item => item.value == value);	
-			countryName = index >= 0? tempcountries[index].label: null;
-			
-			//Sneha-Logic to fetch the State Name through state code and pass it to the controller
-			var tempstates = component.get("v.states"),
-			statevalue = component.find("stateList").get("v.value"),		
-			stateindex = tempstates.findIndex(item => item.value == statevalue);	
-			stateName = stateindex >= 0? tempstates[stateindex].label: null;
-			//if(component.get('v.personWrapper.zip')!=null)
-			zipcode = component.get('v.personWrapper.zip');		
-		}	
-		
-        var secondLangKey = component.get('v.secondLangKey');		
+        var languageKey   = component.get('v.languageKey');
+        var secondLangKey = component.get('v.secondLangKey');
         var thirdLangKey  = component.get('v.thirdLangKey');
         var localeKey     = component.get('v.localeKey');
         var timezoneKey   = component.get('v.timezoneKey');
         component.find('spinner').show();
-		
         communityService.executeAction(component, 'changeLanguage', {
             languageKey:   languageKey,
             secondLangKey: secondLangKey,
             thirdLangKey:  thirdLangKey,
             localeKey:     localeKey,
-            timezoneKey:   timezoneKey,
-			countryName:   countryName,
-			stateName:	   stateName,
-			zipcode:	   zipcode
+            timezoneKey:   timezoneKey
         }, function () {
             location.reload()
         }, function () {
             component.find('spinner').hide();
         })
-    },
-
-    //Sneha-Method to fetch the corresponding states based on the country selected
-	updateStates: function (component, event, helper){		
-		var statesByCountryMap = component.get('v.statesByCountryMap');		
-		var countryCode = component.get("v.countryKey");		
-		component.set('v.states', statesByCountryMap[countryCode]);
-	}
-
-
+    }
 })
