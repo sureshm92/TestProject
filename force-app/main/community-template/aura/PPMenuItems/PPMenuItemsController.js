119034eda1b82b673a8fd1ced3c02d6a60223e53
OSkrynnikova <olga.skrynnikova@customertimes.com>
2020-06-15 18:50:48 +0300

    ppmenuitem

--- force-app/main/community-template/aura/PPMenuItems/PPMenuItemsController.js
+++ force-app/main/community-template/aura/PPMenuItems/PPMenuItemsController.js
@@ -3,6 +3,40 @@
  */
 
 ({
+    doInit: function (component, event, helper) {
+        let communityModes = [
+            {
+                isSelected: false,
+                title: 'Hernya Delegate',
+                subTitle: 'No active studies',
+                subItems: []
+            },
+            {
+                isSelected: false,
+                title: 'Marcus Ray',
+                subItems: [
+                    {
+                        title: 'Study Test 1',
+                        isDelegate: true,
+                        isSelected: true
+                    },
+                    {
+                        title: 'Study Test 2',
+                        isDelegate: true,
+                        isSelected: false
+                    },
+                    {
+                        title: 'Study Test 3',
+                        isDelegate: true,
+                        isSelected: false
+                    }
+                ]
+            },
+
+        ];
+        component.set('v.allModes', communityModes);
+    },
+
     doOnClick: function (component, event) {
         let onclickEvent = component.getEvent('onclick');
         onclickEvent.setParam('source', event.getSource());
