e41c60a400da050e85eb4fdb39bdd643538cbde1
OSkrynnikova <olga.skrynnikova@customertimes.com>
2020-06-17 19:00:11 +0300

    cmp3collapsed

--- force-app/main/community-template/aura/UnifiedStructurePP_RH/UnifiedStructurePP_RHController.js
+++ force-app/main/community-template/aura/UnifiedStructurePP_RH/UnifiedStructurePP_RHController.js
@@ -4,67 +4,75 @@
 
 ({
     doInit: function (component, event, helper) {
-        let communityModes = [
-            {
-                isPPItemsCollapsed: false,
-                isRHItemsCollapsed: false,
-                isPPItemsSelected: false,
-                isRHItemsSelected: true,
-                ppModeItems: [
-                    {
-                        title: 'Hernya Delegate',
-                        isDelegate: false,
-                        subItems: [
-                            {
-                                title: 'Hernya Delegate',
-                                subTitle: 'No active studies',
-                                isDelegate: true,
-                                isSelected: true
-                            }
-                        ]
-                    }
-                ],
-                rhModeItems: [
-                    {
-                        isGroup: false,
-                        isSelected: false,
-                        isCollapsed: false,
-                        title: 'View as Investigative Site',
-                        itemType: 'PI',
-                        itemId: '',
-                        subItems: []
-                    },
-                    {
-                        isGroup: true,
-                        isSelected: true,
-                        isCollapsed: false,
-                        title: 'View as Referring Provider',
-                        itemType: 'RP',
-                        itemId: '',
-                        subItems: [
-                            {
-                                isGroup: false,
-                                isSelected: true,
-                                isCollapsed: false,
-                                title: 'Dr. Oliver Scott',
-                                itemType: 'RP',
-                                itemId: '',
-                                subItems: []
-                            },
-                            {
-                                isGroup: false,
-                                isSelected: false,
-                                isCollapsed: false,
-                                title: 'Dr. Lana Liama',
-                                itemType: 'RP',
-                                itemId: '0912321324234',
-                                subItems: []
-                            }
-                        ]
-                    }
-                ]
-            }
-        ];
+        let communityModes = {
+            isPPItemsCollapsed: false,
+            isRHItemsCollapsed: false,
+            isPPItemsSelected: false,
+            isRHItemsSelected: true,
+            ppModeItems: [
+                {
+                    title: 'Hernya Delegate',
+                    isDelegate: false,
+                    subItems: [
+                        {
+                            title: 'Hernya Delegate',
+                            subTitle: 'No active studies',
+                            isDelegate: true,
+                            isSelected: true
+                        }
+                    ]
+                }
+            ],
+            rhModeItems: [
+                {
+                    isGroup: false,
+                    isSelected: false,
+                    isCollapsed: false,
+                    title: 'View as Investigative Site',
+                    itemType: 'PI',
+                    itemId: '',
+                    subItems: []
+                },
+                {
+                    isGroup: true,
+                    isSelected: true,
+                    isCollapsed: false,
+                    title: 'View as Referring Provider',
+                    itemType: 'RP',
+                    itemId: '',
+                    subItems: [
+                        {
+                            isGroup: false,
+                            isSelected: true,
+                            isCollapsed: false,
+                            title: 'Dr. Oliver Scott',
+                            itemType: 'RP',
+                            itemId: '',
+                            subItems: []
+                        },
+                        {
+                            isGroup: false,
+                            isSelected: false,
+                            isCollapsed: false,
+                            title: 'Dr. Lana Liama',
+                            itemType: 'RP',
+                            itemId: '0912321324234',
+                            subItems: []
+                        }
+                    ]
+                }
+            ]
+        };
         component.set('v.communityModes', communityModes);
-    }
+    },
+    doToggleGroup: function (component, event, helper) {
+        let communityModes = component.get('v.communityModes');
+        let selectedKey = event.currentTarget.dataset.groupId;
+        let ppstr = 'pp';
+        let rhstr = 'rh';
+        if(selectedKey == ppstr) communityModes.isPPItemsCollapsed = !communityModes.isPPItemsCollapsed;
+        if(selectedKey == rhstr) communityModes.isRHItemsCollapsed = !communityModes.isRHItemsCollapsed;
+        component.set('v.communityModes', communityModes);
+    },
+
 });
