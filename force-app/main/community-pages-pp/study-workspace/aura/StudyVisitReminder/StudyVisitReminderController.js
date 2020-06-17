9b741e849ced62044a4763f92fd33405ee5d4b70
Pijush Kar <pijush.kar@quintiles.com>
2020-06-17 13:45:32 +0530

    WIP: PEH-2251 C5

--- force-app/main/community-pages-pp/study-workspace/aura/StudyVisitReminder/StudyVisitReminderController.js
+++ force-app/main/community-pages-pp/study-workspace/aura/StudyVisitReminder/StudyVisitReminderController.js
@@ -1,78 +1,45 @@
 ({
     doInit: function (component, event, helper) {
-        var taskId = component.get('v.taskId');
-        var isNewTask = component.get('v.isNewTask')
-        if (!communityService.isDummy()) {
-            component.find('spinner').show();
-            communityService.executeAction(component, 'getTaskEditData', {
-                taskId: isNewTask ? null : taskId
-            }, function (wrapper) {
-                component.set('v.initData', wrapper);
-                component.set('v.isEnrolled', wrapper.isEnrolled);
-                component.set('v.isEmailReminderSet', wrapper.isEmailReminderSet);
-                component.set('v.isSMSReminderSet', wrapper.isSMSReminderSet);
-
-                if ((wrapper.isEmailReminderSet || wrapper.isSMSReminderSet) && wrapper.reminderDate) {
-                    component.set('v.reminderDateEnabled', true);
-                    if (wrapper.activityDate) {
-                        component.set('v.frequencyEnabled', true);
-                    }
-                }
-
-                component.set('v.taskTypeList', wrapper.taskTypeList);
-
-                var task = wrapper.task;
-                if (isNewTask) {
-                    if (wrapper.activityDate && wrapper.reminderDate) {
-                        var due = moment(wrapper.activityDate, 'YYYY-MM-DD');
-                        var reminder = moment(wrapper.reminderDate, 'YYYY-MM-DD');
-                        if (!due.isSame(reminder)) {
-                            if (due.diff(reminder, 'days') === 1) {
-                                component.set('v.frequencyMode', 'Day_Before');
-                                component.set('v.reminderDateEnabled', false);
-                            }
-                        }
-                    }
-
-                    var isOwner = task.OwnerId === task.CreatedById;
-                    component.set('v.owner', isOwner);
-                    component.set('v.isEditable', isOwner && task.Status !== 'Completed');
-                } else {
-                    component.set('v.isEditable', true);
-
-                    task.Status = 'Open';
-                    task.Task_Type__c = 'Not Selected';
-                }
-                component.set('v.task', task);
-
-                var visitId = component.get('v.visitId');
-                if (!$A.util.isUndefinedOrNull(visitId)) {
-                    component.set('v.task.Patient_Visit__c', visitId);
-                }
-
-                component.set('v.jsonState', JSON.stringify(wrapper) + '' + JSON.stringify(task));
-                component.set('v.isValidFields', true);
-                component.find('spinner').hide();
-            });
-        } else {
-            component.find('builderStub').setPageName(component.getName());
-        }
-        component.find('reminderModal').show();
+        helper.initialize(component);
     },
 
     doCancel: function (component, event, helper) {
-        component.find('reminderModal').hide();
+        helper.hideModal(component);
     },
 
     doSave: function (component, event, helper) {
 
+
     },
 
     doIgnore: function (component, event, helper) {
-
+        helper.updateTaskStatus(component, helper, 'ignoreTask');
     },
 
     doMarkComplete: function (component, event, helper) {
+        helper.updateTaskStatus(component, 'markAsCompleted');
+    },
 
+    doChangeReminderDate: function (component, event, helper) {
+        if (freq === 'Day_Before') {
+            $A.enqueueAction(component.get('c.setDueDate'));
+        }
+    },
+
+    setDueDate: function (component, event, helper) {
+        var reminderDate = moment(component.get('v.initData.reminderDate'), 'YYYY-MM-DD');
+        reminderDate.add(1, 'days');
+        component.set('v.initData.activityDate', reminderDate.format('YYYY-MM-DD'));
+
+    },
+
+    validateFields: function (component, event, helper) {
+        if (component.get('v.initData') && component.get('v.initData.createdByAdmin')) return;
+
+        var allValid = component.find('dateField').reduce(function (validSoFar, inputCmp) {
+            return validSoFar && inputCmp.checkValidity();
+        }, true);
+
+        component.set('v.isValidFields', allValid);
     }
 })
