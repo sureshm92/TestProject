731a44a3a701399ec059e082fa7ffce5e7a2d9d3
Chandra Mohan <kanchi.force1@gmail.com>
2020-06-27 17:01:45 +0530

    CustomLables

--- force-app/main/default/aura/SearchReferralNetwork/SearchReferralNetworkController.js
+++ force-app/main/default/aura/SearchReferralNetwork/SearchReferralNetworkController.js
@@ -1,41 +1,40 @@
 ({
-    doInit: function (component, event, helper) {
+	 doInit: function (component, event, helper) {
+          
         communityService.executeAction(component, 'getReferralNetworkRecords', {
             sObjectType: component.get("v.sObjectType")
         }, function (returnValue) {
-            var selectedPills = component.get('v.selectedPills');
-            var selPills = [];
-            for(var i in returnValue) {
-                selPills.push(returnValue[i]);
-                selPills[i].isSelected = true;
-                selectedPills[selPills[i].Id] =  selPills[i];
-             }
-            component.set('v.selectedPills', selectedPills);
+            console.log('returnValue' + JSON.stringify(returnValue));
+      
             component.set('v.records', returnValue);
+            
         });  
     },
     
-    bulkSearch: function (component, event, helper) {
+      bulkSearch: function (component, event, helper) {
         helper.doSearch(component, event, helper);
         
     },
-    closeSearch: function (component, event, helper) {
-        var ddown = component.find('dropdown');
-        $A.util.addClass(ddown, 'slds-hide');
-        component.find('searchInput').set('v.value', '');
-     },
+     closeSearch: function (component, event, helper) {
+         var ddown = component.find('dropdown');
+         $A.util.addClass(ddown, 'slds-hide');
+           component.find('searchInput').set('v.value', '');
+          // component.set('v.records', null);
+        
+    },
     
-    handleChange: function (component, event, helper) {
+      handleChange: function (component, event, helper) {
         helper.changeCheckBox(component, event);
     },
     
-    handleRemoveOnly: function (component, event,helper) {
+      handleRemoveOnly: function (component, event,helper) {
         event.preventDefault();
         helper.handleClearPill(component,event,helper); 
-        
-    }
+          var ddown = component.find('dropdown');
+          $A.util.toggleClass(ddown, 'slds-hide');  
+        }
     ,
-    
+   
     
     
 })
