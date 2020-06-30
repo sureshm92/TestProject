612830d2259c6aafa88e6b3703cb29fff30c11ea
Nargiz Mamedova <nargiz.mamedova@customertimes.com>
2020-06-30 19:58:58 +0300

    switcher blur

--- force-app/main/community-template/mode-switcher/aura/ModeHeaderMenu/ModeHeaderMenuController.js
+++ force-app/main/community-template/mode-switcher/aura/ModeHeaderMenu/ModeHeaderMenuController.js
@@ -5,10 +5,12 @@
 ({
     doMenuExpand: function(component, event, helper){
         component.set('v.isOpened', true);
+        component.getEvent('onshow').fire();
     },
 
     doMenuCollapse: function (component, event, helper) {
         component.set('v.isOpened', false);
+        component.getEvent('onblur').fire();
     },
 
     doClose: function(component){
