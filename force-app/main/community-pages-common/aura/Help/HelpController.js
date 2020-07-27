5088c567370ad48c6280b95ec3b9162ea9e00f31
Ranjit Ravindranath <ranjit.ravindranath@quintiles.com>
2020-07-27 12:19:41 +0530

    RTL

--- force-app/main/community-pages-common/aura/Help/HelpController.js
+++ force-app/main/community-pages-common/aura/Help/HelpController.js
@@ -1,7 +1,14 @@
 ({
     doInit: function (component, event, helper) {
         if (!communityService.isInitialized()) return;
-
+        var cmpTarget = component.find('borderLine');
+        if(component.get("v.isArabic")){
+        	$A.util.addClass(cmpTarget, 'h-right-col');
+            $A.util.removeClass(cmpTarget, 'h-left-col');
+        }else{
+            $A.util.addClass(cmpTarget, 'h-left-col');
+            $A.util.removeClass(cmpTarget, 'h-right-col');
+        }
         if(!communityService.isDummy()) {
             component.set('v.userMode', communityService.getUserMode());
             communityService.executeAction(component, 'getHelpText', null, function (response) {
