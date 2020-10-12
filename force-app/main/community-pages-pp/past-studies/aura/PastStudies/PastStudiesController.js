3b7750beb7bda6489620c22f08bea02a8e115736
Pijush Kar <pijush.kar@quintiles.com>
2020-10-12 15:05:56 +0530

    Hide jsPDF reports in Mobile Publisher

--- force-app/main/community-pages-pp/past-studies/aura/PastStudies/PastStudiesController.js
+++ force-app/main/community-pages-pp/past-studies/aura/PastStudies/PastStudiesController.js
@@ -1,19 +1,31 @@
 ({
-    doInit: function (component, event, helper) {
-        if (!communityService.isInitialized()) return;
+	doInit: function (component, event, helper) {
+		if (!communityService.isInitialized()) return;
 
-        if (!communityService.isDummy()) {
-            component.find('spinner').show();
-            communityService.executeAction(component, 'getPastStudyRecords', null,
-                function (pastStudies) {
-                    component.set('v.pastStudiesList', pastStudies);
-                    component.set('v.isDelegateMode', communityService.getCurrentCommunityMode().currentDelegateId);
-                    component.set('v.initialized', true);
-                    component.find('spinner').hide();
-                    if (!communityService.getCurrentCommunityMode().hasPastStudies) communityService.navigateToHome();
-                });
-        } else {
-            component.find('builderStub').setPageName(component.getName());
-        }
-    }
+		if (!communityService.isDummy()) {
+			component.find('spinner').show();
+			component.set(
+				'v.isMobileApp',
+				communityService.isCurrentSessionMobileApp()
+			);
+			communityService.executeAction(
+				component,
+				'getPastStudyRecords',
+				null,
+				function (pastStudies) {
+					component.set('v.pastStudiesList', pastStudies);
+					component.set(
+						'v.isDelegateMode',
+						communityService.getCurrentCommunityMode().currentDelegateId
+					);
+					component.set('v.initialized', true);
+					component.find('spinner').hide();
+					if (!communityService.getCurrentCommunityMode().hasPastStudies)
+						communityService.navigateToHome();
+				}
+			);
+		} else {
+			component.find('builderStub').setPageName(component.getName());
+		}
+	}
 });
--- force-app/main/default/aura/TrialMatchPageLearnMoreLink/TrialMatchPageLearnMoreLinkController.js
+++ force-app/main/default/aura/TrialMatchPageLearnMoreLink/TrialMatchPageLearnMoreLinkController.js
@@ -1,18 +1,27 @@
 /**
- * Created by Enugothula Srinath 
+ * Created by Enugothula Srinath
  * Date-21/05/2020
  */
 
-
 ({
-    doInit: function(component,event, helper){
-        component.set('v.initialized', true);
-    },
-    doGenerateReport: function (component, event, helper) {
-        helper.uploadReportData(component, function () {
-            window.setTimeout($A.getCallback(function() {
-                    helper.generateLearnMorePDF(component);
-            }), 100);
-        })
-    }
+	doInit: function (component, event, helper) {
+		if (!communityService.isInitialized()) return;
+		else {
+			component.set('v.initialized', true);
+			component.set(
+				'v.isMobileApp',
+				communityService.isCurrentSessionMobileApp()
+			);
+		}
+	},
+	doGenerateReport: function (component, event, helper) {
+		helper.uploadReportData(component, function () {
+			window.setTimeout(
+				$A.getCallback(function () {
+					helper.generateLearnMorePDF(component);
+				}),
+				100
+			);
+		});
+	}
 });
