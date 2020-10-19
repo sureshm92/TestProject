844102afad9851f60d951ad590f7fbbce7a8f7f2
Pijush Kar <pijush.kar@quintiles.com>
2020-10-19 17:08:03 +0530

    PEH-2636: Mobile App Cookie and Privacy Policy

--- force-app/main/default/aura/TermsAndConditions/TermsAndConditionsHelper.js
+++ force-app/main/default/aura/TermsAndConditions/TermsAndConditionsHelper.js
@@ -5,13 +5,18 @@
     goBack: function (component) {
         var retString = communityService.getUrlParameter('ret');
         var isPortalTC = component.get('v.isPortalTC');
-        if(retString !== 'terms-and-conditions' && retString || retString === ''){
+        if (
+            (retString !== 'terms-and-conditions' && retString) ||
+            retString === ''
+        ) {
             var retPage = communityService.getRetPage(retString);
             communityService.navigateToPage(retPage);
-        }else{
-            if(!isPortalTC && component.get('v.ctpId')){
-                communityService.navigateToPage('study-workspace?id=' + component.get('v.ctpId'));
-            }else{
+        } else {
+            if (!isPortalTC && component.get('v.ctpId')) {
+                communityService.navigateToPage(
+                    'study-workspace?id=' + component.get('v.ctpId')
+                );
+            } else {
                 communityService.navigateToPage('');
             }
         }
@@ -20,19 +25,20 @@
     hideOkButton: function (component, event, helper) {
         var retString = communityService.getUrlParameter('ret');
         var isPortalTC = component.get('v.isPortalTC');
-        if(retString !== 'terms-and-conditions' && retString || retString === ''){
-            component.set("v.isUserLoggedIn", true);
-        }else{
-            if(!isPortalTC && component.get('v.ctpId')){
-                component.set("v.isUserLoggedIn", true);
-            }
-            else if(!communityService.isDummy()){
-                component.set("v.isUserLoggedIn", true);
-            }
-            else{
-                component.set("v.isUserLoggedIn", false);
+        if (
+            (retString !== 'terms-and-conditions' && retString) ||
+            retString === ''
+        ) {
+            component.set('v.isUserLoggedIn', true);
+        } else {
+            if (!isPortalTC && component.get('v.ctpId')) {
+                component.set('v.isUserLoggedIn', true);
+            } else if (!communityService.isDummy()) {
+                component.set('v.isUserLoggedIn', true);
+            } else {
+                component.set('v.isUserLoggedIn', false);
             }
         }
     }
     //@Krishna Mahto - PEH-2450 - End
-})
+});
