2b5208288afec67ec312674b04260df7f29beea2
Deeksha <deeksha.d@quintiles.com>
2020-11-25 17:56:56 +0530

    Date format inconsistency

--- force-app/main/community-pages-pp/study-workspace/aura/VisitResultsPanel/VisitResultsPanelController.js
+++ force-app/main/community-pages-pp/study-workspace/aura/VisitResultsPanel/VisitResultsPanelController.js
@@ -1,30 +1,39 @@
-(
-    {
-        doInit : function(component, event, helper) {
-
-            component.set('v.initialized', false);
-            component.set('v.displayPanel', true);
-            let spinner = component.find('mainSpinner');
-            if(spinner) {
-                spinner.show();
-            }
-            let visitResultsShares = JSON.stringify(component.get('v.initData.visitResultsShares'));
-            communityService.executeAction(component, 'getVisitResultsContainer', {
-                    visitResultGroupName: component.get('v.visitResultGroupName'),
-                    visitResultsShares: visitResultsShares
-                }, function (returnValue) {
-                    if(returnValue === null) {
-                        component.set('v.displayPanel', false);
+({
+    doInit: function (component, event, helper) {
+        component.set('v.initialized', false);
+        component.set('v.displayPanel', true);
+        let spinner = component.find('mainSpinner');
+        if (spinner) {
+            spinner.show();
+        }
+        let visitResultsShares = JSON.stringify(component.get('v.initData.visitResultsShares'));
+        communityService.executeAction(
+            component,
+            'getVisitResultsContainer',
+            {
+                visitResultGroupName: component.get('v.visitResultGroupName'),
+                visitResultsShares: visitResultsShares
+            },
+            function (returnValue) {
+                if (returnValue === null) {
+                    component.set('v.displayPanel', false);
+                } else {
+                    component.set('v.visitResultsContainer', returnValue);
+                    if (
+                        component.get('v.visitResultsContainer.visitDate') ==
+                        $A.get('$Label.StudyVisit_Information_Not_Available')
+                    ) {
+                        component.set('v.isVisitResultAvailable', false);
                     } else {
-                        component.set('v.visitResultsContainer', returnValue);
-                        component.set('v.initialized', true);
+                        component.set('v.isVisitResultAvailable', true);
                     }
-                    let spinner = component.find('mainSpinner');
-                    if(spinner) {
-                        spinner.hide();
-                    }
+                    component.set('v.initialized', true);
                 }
-            );
-        },
+                let spinner = component.find('mainSpinner');
+                if (spinner) {
+                    spinner.hide();
+                }
+            }
+        );
     }
-);
+});
