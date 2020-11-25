ed4de057ffff5c72ead2e0a0d5d28e007028cfd7
Deeksha <deeksha.d@quintiles.com>
2020-11-25 14:05:33 +0530

    Data Skew issue and Visit Reminder Issue

--- force-app/main/default/aura/StudySiteCardForRP/StudySiteCardForRPController.js
+++ force-app/main/default/aura/StudySiteCardForRP/StudySiteCardForRPController.js
@@ -7,14 +7,22 @@
         var siteWrapper = cmp.get('v.siteWrapper');
         var trialId = siteWrapper.site.Clinical_Trial_Profile__c;
         var hcpeId = event.target.dataset.hcpeId;
-        communityService.navigateToPage('referring?id=' + trialId + (hcpeId ? '&hcpeid=' + hcpeId : ''));
+        var siteId = event.target.dataset.siteId;
+
+        communityService.navigateToPage(
+            'referring?id=' +
+                trialId +
+                (hcpeId ? '&hcpeid=' + hcpeId : '') +
+                (siteId ? '&siteid=' + siteId : '')
+        );
     },
 
     doMyPatients: function (cmp, event, helper) {
         var siteWrapper = cmp.get('v.siteWrapper');
         var trialId = siteWrapper.site.Clinical_Trial_Profile__c;
         var siteId = event.target.dataset.siteId;
-        communityService.navigateToPage('my-patients?id=' + trialId + (siteId ? '&siteId=' + siteId : ''));
-    },
-
+        communityService.navigateToPage(
+            'my-patients?id=' + trialId + (siteId ? '&siteId=' + siteId : '')
+        );
+    }
 });
