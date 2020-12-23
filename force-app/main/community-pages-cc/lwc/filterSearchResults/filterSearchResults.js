269a6f47e60fd3fe63b69c5732a2fa523b742f3f
Pijush Kar <pijush.kar@quintiles.com>
2020-12-23 15:37:43 +0530

    Updated components

--- force-app/main/community-pages-cc/lwc/filterSearchResults/filterSearchResults.js
+++ force-app/main/community-pages-cc/lwc/filterSearchResults/filterSearchResults.js
@@ -1,6 +1,5 @@
 import { LightningElement, track, wire } from 'lwc';
 import formFactor from '@salesforce/client/formFactor';
-import { CurrentPageReference, NavigationMixin } from 'lightning/navigation';
 import { registerListener, unregisterAllListeners } from 'c/pubSub';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-export default class SiteListPage extends NavigationMixin(LightningElement) {}
+export default class FilterSearchResults extends LightningElement {}
