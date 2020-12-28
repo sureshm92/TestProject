af9953c1a7c214c4d6efaa701686bba6cba5f04e
Pijush Kar <pijush.kar@quintiles.com>
2020-12-28 19:19:12 +0530

    Search Panel - Structure and JS

--- force-app/main/community-pages-cc/aura/SiteSearchPanel/SiteSearchPanelHelper.js
+++ force-app/main/community-pages-cc/aura/SiteSearchPanel/SiteSearchPanelHelper.js
@@ -1,5 +1,9 @@
 ({
-    helperMethod : function() {
-
+    doUpdateSelectionHelper: function (component) {
+        let option = component.get('v.selectedSearchOption');
+        if (option !== '') {
+            component.set('v.sectionDisabled', false);
+            component.set('v.searchTextPlaceHolder', $A.get('$Label.c.CC_SearchPlaceholder'));
+        }
     }
-})
+});
