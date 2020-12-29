fd961a753f1cac91a866a638bca762585395e9f1
Pijush Kar <pijush.kar@quintiles.com>
2020-12-29 20:58:09 +0530

    REF-2776 - Apex Structure, All Markup Structure

--- force-app/main/community-pages-cc/aura/SiteListPage/SiteListPageController.js
+++ force-app/main/community-pages-cc/aura/SiteListPage/SiteListPageController.js
@@ -3,14 +3,11 @@
         if (!communityService.isInitialized()) return;
         if (!communityService.isDummy()) {
             let spinner = component.find('mainSpinner');
-            spinner.show();
-            component.set('v.userMode', communityService.getUserMode());
-            component.set('v.isInitialized', true);
             let searchList = [
-                { label: $A.get('$Label.c.CC_StudySiteName'), value: 'Study Site Name' },
-                { label: $A.get('$Label.c.CC_ProtocolNumber'), value: 'Protocol Number' },
-                { label: $A.get('$Label.c.CC_SiteNumber'), value: 'Site Number' },
-                { label: $A.get('$Label.c.CC_PI_Name'), value: 'PI Name' }
+                { label: $A.get('$Label.c.CC_StudySiteName'), value: 'Name' },
+                { label: $A.get('$Label.c.CC_ProtocolNumber'), value: 'Protocol_ID__c' },
+                { label: $A.get('$Label.c.CC_SiteNumber'), value: 'Study_Site_Number__c' },
+                { label: $A.get('$Label.c.CC_PI_Name'), value: 'Principal_Investigator__r.Name' }
             ];
             let sortVariants = [
                 { label: 'Site Alphabetical (a-z)', value: 'Name ASC' },
@@ -18,27 +15,41 @@
                 { label: 'Site Number (Ascending)', value: 'Study_Site_Number__c ASC' },
                 { label: 'Site Number (Descending)', value: 'Study_Site_Number__c DESC' }
             ];
+            let resultSet = [];
+            spinner.show();
+            component.set('v.userMode', communityService.getUserMode());
+            component.set('v.isInitialized', true);
             component.set('v.searchList', searchList);
             component.set('v.sortVariants', sortVariants);
             component.set('v.sectionDisabled', true);
             component.set('v.searchTextPlaceHolder', $A.get('$Label.c.CC_Btn_Search'));
             component.set('v.sortType', 'Name ASC');
             component.set('v.searchText', '');
+            component.set('v.resultSet', resultSet);
+            component.set('v.filterStudyName', '');
+            component.set('v.filterCountry', '');
             spinner.hide();
         } else {
             component.find('builderStub').setPageName(component.getName());
         }
     },
-    doExactSearch: function (component, event, helper) {
+    doDatabaseSearch: function (component, event, helper) {
+        debugger;
         let params = event.getParam('arguments');
         if (params && params.selectedSearchOption && params.searchText && params.sortType) {
-            window.alert('doExactSearch' + JSON.stringify(params));
-        }
-    },
-    doPartialSearch: function (component, event, helper) {
-        let params = event.getParam('arguments');
-        if (params && params.selectedSearchOption && params.searchText && params.sortType) {
-            window.alert('doPartialSearch' + JSON.stringify(params));
+            console.log('doDatabaseSearch' + JSON.stringify(params));
+            communityService.executeAction(
+                component,
+                'getSearchResults',
+                {
+                    searchOption: params.selectedSearchOption,
+                    searchText: params.searchText,
+                    sortType: params.sortType
+                },
+                function (siteList) {
+                    component.set('v.resultSet', siteList);
+                }
+            );
         }
     }
 });
