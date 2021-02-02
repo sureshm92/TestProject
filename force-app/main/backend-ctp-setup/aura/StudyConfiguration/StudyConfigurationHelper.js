a04f576ffaf32d36c9034c29557e692a377e6197
Sravani <sravani.dasari@quintiles.com>
2021-02-02 19:16:24 +0530

    PEH-3061

--- force-app/main/backend-ctp-setup/aura/StudyConfiguration/StudyConfigurationHelper.js
+++ force-app/main/backend-ctp-setup/aura/StudyConfiguration/StudyConfigurationHelper.js
@@ -12,18 +12,30 @@
             component.find('vsToggle').set('v.checked', !stValue);
         }
 
+        if(source === 'saveDelayDays'){
+            component.set('v.delay_days', component.get("v.ctp.Delayed_No_Of_Days__c"));
+        }
+        else{
+            component.set('v.ctp.Delayed_No_Of_Days__c', component.get("v.delay_days"));
+        }
         component.find('spinner').show();
-        communityService.executeAction(
-            component,
-            'saveChanges',
-            {
-                ctp: component.get('v.ctp')
-            },
-            function () {
-                component.find('spinner').hide();
-                communityService.showSuccessToast('Success', 'Study Configuration setting saved!');
-            }
-        );
+        if((source === 'saveDelayDays' && component.find('delayedDays').reportValidity()) || source !== 'saveDelayDays'){
+            communityService.executeAction(
+                component,
+                'saveChanges',
+                {
+                    ctp: component.get('v.ctp')
+                },
+                function () {
+                    component.find('spinner').hide();
+                    communityService.showSuccessToast('Success', 'Study Configuration setting saved!');
+                }
+        	);
+        }
+        else{
+            component.find('spinner').hide();
+          //  communityService.showToast('Error', 'error', 'The incentive program already exists');
+        }
         if (source === 'tasksToggle') {
             $A.get('e.force:refreshView').fire();
             appEvent.fire();
