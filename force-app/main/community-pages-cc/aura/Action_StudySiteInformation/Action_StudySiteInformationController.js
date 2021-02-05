a7dcfea80009a4ae6a1af9e3f50bb43f3284d4d5
Akanksa <akanksha.gupta@quintiles.com>
2021-02-05 17:46:47 +0530

    REF-3071

--- force-app/main/community-pages-cc/aura/Action_StudySiteInformation/Action_StudySiteInformationController.js
+++ force-app/main/community-pages-cc/aura/Action_StudySiteInformation/Action_StudySiteInformationController.js
@@ -30,73 +30,93 @@
     },
     
     doSave: function (component, event, helper) {
-        component.find('Spinnerpopup').show();
-        var siteWrapper = component.get('v.studyInformation');
-        var callDisp = JSON.stringify(component.get('v.CD'));
-        var result = callDisp.slice(1,-1);
-        communityService.executeAction(
-            component,
-            'saveSSChanges',
-            { 
-                studySiteInfo: JSON.stringify(siteWrapper),
-                accId : component.get('v.editedAccount'),
-                callDisp: result,
-                newCall: component.get('v.newCall')
-            },
-            function () {
-                communityService.showToast(
-                    'success',
-                    'success',
-                    $A.get('$Label.c.CC_Success_Save_Message')
-                );
-                component.find('childCmp').RefreshCDsection();
-            },
-            null,
-            function () {
-                component.find('childCmp').RefreshCDsection();
-                component.find('Spinnerpopup').hide();
-            }
-        );
+        if(component.get('v.studyInformation').supressPIEmail &&
+           component.get('v.studyInformation').receivePIEmail){
+            communityService.showToast(
+                'error',
+                'error',
+                $A.get('$Label.c.CC_Suppress_PI_Email')
+            );
+        }
+        else{
+            component.find('Spinnerpopup').show();
+            var siteWrapper = component.get('v.studyInformation');
+            var callDisp = JSON.stringify(component.get('v.CD'));
+            var result = callDisp.slice(1,-1);
+            communityService.executeAction(
+                component,
+                'saveSSChanges',
+                { 
+                    studySiteInfo: JSON.stringify(siteWrapper),
+                    accId : component.get('v.editedAccount'),
+                    callDisp: result,
+                    newCall: component.get('v.newCall')
+                },
+                function () {
+                    communityService.showToast(
+                        'success',
+                        'success',
+                        $A.get('$Label.c.CC_Success_Save_Message')
+                    );
+                    component.find('childCmp').RefreshCDsection();
+                },
+                null,
+                function () {
+                    component.find('childCmp').RefreshCDsection();
+                    component.find('Spinnerpopup').hide();
+                }
+            );
+        }
     },
     
     doSaveandExit: function (component, event, helper) {
-        component.find('Spinnerpopup').show();
-        var siteWrapper = component.get('v.studyInformation');
-        var callDisp = JSON.stringify(component.get('v.CD'));
-        var result = callDisp.slice(1,-1);
-        communityService.executeAction(
-            component,
-            'saveSSChanges',
-            { 
-                studySiteInfo: JSON.stringify(siteWrapper),
-                accId : component.get('v.editedAccount'),
-                callDisp: result,
-                newCall: component.get('v.newCall')
-            },
-            function () {
-                communityService.showToast(
-                    'success',
-                    'success',
-                    $A.get('$Label.c.CC_Success_Save_Message')
-                );
-                component.set('v.CmpOpen',false);
-                var cmpEvts = component.getEvent("CloseSSInfoEvent"); 
-                cmpEvts.fire(); 
-            },
-            null,
-            function () {
-                component.find('Spinnerpopup').hide();
-                component.find('childCmp').RefreshCD();
-                component.find('dialog').cancel();
-                component.set('v.CmpOpen',false);
-                //component.find('dialog').hide();
-                //var cmpEvent = component.getEvent("RefreshCmpEvent"); 
-                //cmpEvent.fire();
-                var cmpEvents = component.getEvent("CloseSSInfoEvent"); 
-                cmpEvents.fire(); 
-                
-            }
-        );
+        if(component.get('v.studyInformation').supressPIEmail &&
+           component.get('v.studyInformation').receivePIEmail){
+            communityService.showToast(
+                'error',
+                'error',
+                $A.get('$Label.c.CC_Suppress_PI_Email')
+            );
+        }
+        else{
+            component.find('Spinnerpopup').show();
+            var siteWrapper = component.get('v.studyInformation');
+            var callDisp = JSON.stringify(component.get('v.CD'));
+            var result = callDisp.slice(1,-1);
+            communityService.executeAction(
+                component,
+                'saveSSChanges',
+                { 
+                    studySiteInfo: JSON.stringify(siteWrapper),
+                    accId : component.get('v.editedAccount'),
+                    callDisp: result,
+                    newCall: component.get('v.newCall')
+                },
+                function () {
+                    communityService.showToast(
+                        'success',
+                        'success',
+                        $A.get('$Label.c.CC_Success_Save_Message')
+                    );
+                    component.set('v.CmpOpen',false);
+                    var cmpEvts = component.getEvent("CloseSSInfoEvent"); 
+                    cmpEvts.fire(); 
+                },
+                null,
+                function () {
+                    component.find('Spinnerpopup').hide();
+                    component.find('childCmp').RefreshCD();
+                    component.find('dialog').cancel();
+                    component.set('v.CmpOpen',false);
+                    //component.find('dialog').hide();
+                    //var cmpEvent = component.getEvent("RefreshCmpEvent"); 
+                    //cmpEvent.fire();
+                    var cmpEvents = component.getEvent("CloseSSInfoEvent"); 
+                    cmpEvents.fire(); 
+                    
+                }
+            );
+        }
     },
     
     doClosePopups: function (component, event, helper) {
