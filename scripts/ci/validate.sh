9007a9c1243340e112d7e4f903f34f3dfdd94de9
Vijaya priya <vijaya.priya@quintiles.com>
2021-02-09 11:47:53 -0500

    Update validate.sh

--- scripts/ci/validate.sh
+++ scripts/ci/validate.sh
@@ -1,8 +1,7 @@
 # Prepare auth file
 orgAuthVar=$1
 echo "${!orgAuthVar}" > authFile
-
-sfdx force:auth:sfdxurl:store -f authFile -a TargetOrg -d
+echo 'y' | sfdx force:auth:sfdxurl:store -f authFile -a TargetOrg -d
 sfdx --version
 cat ./scripts/ci/.forceignore_validate_exclude >> .forceignore
 sfdx force:source:deploy -p force-app -u TargetOrg -c -w 60
