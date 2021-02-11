225cc96bb4504ead64e20ba688eb69198ac6beec
Chandra Mohan <kanchichandramohan.reddy@quintiles.com>
2021-02-11 20:00:38 +0530

    ref-3011

--- force-app/main/incentive-program/lwc/incentivePage/incentivePage.js
+++ force-app/main/incentive-program/lwc/incentivePage/incentivePage.js
@@ -2,17 +2,18 @@
  * Created by Andrii Kryvolap.
  */
 
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
 
 import getPageEnabled from '@salesforce/apex/IncentiveProgramRemote.getIncentivesPageEnabled';
-
+import getisRTL from '@salesforce/apex/ParticipantVisitsRemote.getIsRTL';
 import incentivesLabel from '@salesforce/label/c.IncentiveProgram_Incentives';
 
 export default class IncentivePage extends NavigationMixin(LightningElement) {
     labels = {
         incentivesLabel
     };
+    @track isRTL;
     connectedCallback() {
         getPageEnabled()
             .then((data) => {
@@ -27,6 +28,13 @@ export default class IncentivePage extends NavigationMixin(LightningElement) {
             })
             .catch((error) => {
                 console.error('Error in getPageEnabled():' + JSON.stringify(error));
+            });
+            getisRTL()
+            .then((data) => {
+                this.isRTL = data === true? 'rtl': '';
+            })
+            .catch(function(error) {
+                console.error('Error: ' + JSON.stringify(error));
             });
     }
 }
