f2863907eabc9e98c7feaea3901bcd3b48842215
sandeep <sandeepkiran.dondapati@quintiles.com>
2021-02-17 21:55:03 +0530

    PEH-3150

--- force-app/main/community-pages-pp/common/aura/ResourceLink/ResourceLinkHelper.js
+++ force-app/main/community-pages-pp/common/aura/ResourceLink/ResourceLinkHelper.js
@@ -4,17 +4,19 @@
 ({
     viewResource: function (resource, languageCode) {
         if (communityService.isMobileSDK()) {
-            communityService.showWarningToast(
+          /**  communityService.showWarningToast(
                 'Warning!',
                 $A.get('$Label.c.Pdf_Not_Available'),
                 100
-            );
+            ); **/
+            if(resource.RecordType.DeveloperName=='Video') communityService.navigateToPage(url);
+            communityService.navigateToPage('pdfdemo?resId=' + resource.Id+'&lang=' + languageCode);
             return;
         }
         var url =
             'resources?resourceType=' + resource.RecordType.DeveloperName + '&resId=' + resource.Id;
         if (languageCode) url += '&lang=' + languageCode;
-        url += '&ret=' + communityService.createRetString();
-        communityService.navigateToPage(url);
+        url += '&ret=' + communityService.createRetString();   
+        communityService.navigateToPage(url); 
     }
 });
