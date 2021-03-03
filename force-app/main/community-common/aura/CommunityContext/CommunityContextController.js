acba742d32c000c8e2ff6ff6f1ef115cc7bb0de9
Chandra Mohan <kanchichandramohan.reddy@quintiles.com>
2021-03-03 13:00:18 +0530

    REF-3201

--- force-app/main/community-common/aura/CommunityContext/CommunityContextController.js
+++ force-app/main/community-common/aura/CommunityContext/CommunityContextController.js
@@ -5,6 +5,7 @@
     doInit: function (component, event, helper) {
         var rtl_language = $A.get('$Label.c.RTL_Languages');
         component.set('v.isRTL', rtl_language.includes(communityService.getLanguage()));
+        component.set('v.language', communityService.getLanguage());
         component.set('v.isMobileApp', communityService.isMobileSDK());
     }
 });
