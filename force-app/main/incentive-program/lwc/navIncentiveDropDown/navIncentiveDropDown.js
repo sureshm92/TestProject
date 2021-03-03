28ee54f5385563731a953e1894c2ee69b633436b
Chandra Mohan <kanchichandramohan.reddy@quintiles.com>
2021-03-03 16:27:08 +0530

    3212

--- force-app/main/incentive-program/lwc/navIncentiveDropDown/navIncentiveDropDown.js
+++ force-app/main/incentive-program/lwc/navIncentiveDropDown/navIncentiveDropDown.js
@@ -4,20 +4,15 @@
 
 import { LightningElement, api, track } from 'lwc';
 import totalLabel from '@salesforce/label/c.IncentiveProgram_Total';
-import totalLabelRTL from '@salesforce/label/c.IncentiveProgram_TotalRTL';
 import lastEarnedLabel from '@salesforce/label/c.IncentiveProgram_Last_Earned';
-import lastEarnedLabelRTL from '@salesforce/label/c.IncentiveProgram_Last_EarnedRTL';
 import ptsLabel from '@salesforce/label/c.IncentiveProgram_Pts';
 import viewDetailsLabel from '@salesforce/label/c.IncentiveProgram_View_Details';
 
 export default class NavIncentiveDropDown extends LightningElement {
     labels = {
         totalLabel,
-        totalLabelRTL,
         lastEarnedLabel,
-        lastEarnedLabelRTL,
         ptsLabel,
-       
         viewDetailsLabel
     };
     @api isRtl;
