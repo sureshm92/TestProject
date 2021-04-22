c66edd62c6072f5a67c3830200caf86ec2f4f20f
Srinath E <enugothula.srinath@quintiles.com>
2021-04-22 11:22:39 +0530

    Prescreen

--- force-app/main/default/aura/Action_ParticipantMrrResult/Action_ParticipantMrrResultHelper.js
+++ force-app/main/default/aura/Action_ParticipantMrrResult/Action_ParticipantMrrResultHelper.js
@@ -13,6 +13,10 @@
                 component.set('v.mrrResult', status);
                 //component.find('dialog').scrollTop();
                 component.get('v.callback')(pe);
+                component.get('v.callback')(pe);
+                var compEvent = component.getEvent("FilterKeep");
+                compEvent.fire();
+
             },
             null,
             function () {
