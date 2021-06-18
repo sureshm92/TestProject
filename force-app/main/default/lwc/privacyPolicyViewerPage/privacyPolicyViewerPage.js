579c242e5dab718ef2075fa2db8cfe46638fe712
Ranjit Ravindranath <ranjit.ravindranath@quintiles.com>
2021-06-18 10:50:26 +0530

    PP page RTL

--- force-app/main/default/lwc/privacyPolicyViewerPage/privacyPolicyViewerPage.js
+++ force-app/main/default/lwc/privacyPolicyViewerPage/privacyPolicyViewerPage.js
@@ -1,32 +1,26 @@
 /**
  * Created by Ranjit Ravindranath 5/20/2021
  */
-import {
-    LightningElement,
-    track,
-    api,
-    wire
-} from 'lwc';
+import { LightningElement, track, api, wire } from 'lwc';
 import RR_COMMUNITY_JS from '@salesforce/resourceUrl/rr_community_js';
-import {
-    loadScript
-} from 'lightning/platformResourceLoader';
+import { loadScript } from 'lightning/platformResourceLoader';
 import downloadPdf from '@salesforce/label/c.Download_PDF_pp';
 import lastUpdatedText from '@salesforce/label/c.Last_Updated_pp_text';
 import ppHeaderLabel from '@salesforce/label/c.Email_Footer_Privacy_Policy'; //Email_Footer_Privacy_Policy
 import ppLabel from '@salesforce/label/c.Footer_Link_Privacy_Policy';
+import rtlLanguages from '@salesforce/label/c.RTL_Languages';
 import headerLabel from '@salesforce/label/c.Privacypolicy_pdf_headers';
 import getPrivacyPolicy from '@salesforce/apex/TermsAndConditionsRemote.getTC';
 import generatePDF from '@salesforce/apex/TermsAndConditionsRemote.generatePDF';
 import LOFI_LOGIN_ICONS from '@salesforce/resourceUrl/Lofi_Login_Icons';
 import formFactor from '@salesforce/client/formFactor';
 
-
 export default class PrivacyPolicyViewer extends LightningElement {
     @track selectedItem = 'reports_recent';
     @track currentContent = 'reports_recent';
     @api isModalOpen;
     @api testing;
+    @track isRTL;
     @track ppRichText;
     @track listOfHeaders = [];
     @track paraList = [];
@@ -37,10 +31,17 @@ export default class PrivacyPolicyViewer extends LightningElement {
     @track headerAndLogoClass;
     @track lastUpdated;
     @track empNames = [];
+    @track outerContainerClass = 'outerContainer';
+    @track bodyWrapper = 'bodyWrapper';
+    @track wrapCss;
+    @track logoClass = 'logo';
+    @track ppHeaderClass = 'ppHeader';
+    @track headerAndLogoClass = 'headerAndLogo';
     @track frmFactor = false;
     currentPageReference = null;
     closePrivacyPolicyTab = false;
     defaultCommunityBoolean = true;
+    lanCode;
     tcId;
     attachment;
 
@@ -63,7 +64,7 @@ export default class PrivacyPolicyViewer extends LightningElement {
     navigateToHomePage(event) {
         var needle = event.currentTarget.dataset.value;
         if (needle.includes("'")) {
-            needle = needle.replace("'", "&#39;");
+            needle = needle.replace("'", '&#39;');
         }
         var newValue = this.ppRichText.replace(needle, needle + '<a data-id="imhere"></a>');
         this.template.querySelector('[data-id="text"]').innerHTML = newValue;
@@ -79,20 +80,18 @@ export default class PrivacyPolicyViewer extends LightningElement {
         });*/
     }
 
-
     saveAsPdf() {
         generatePDF({
-                ppId: this.tcId
-            })
-            .then((result) => {
+            ppId: this.tcId
+        })
+            .then(result => {
                 location.href = result;
             })
-            .catch((error) => {
+            .catch(error => {
                 console.log(JSON.stringify(error));
             });
     }
     renderedCallback() {
-
         this.spinner = this.template.querySelector('c-web-spinner');
         this.spinner.show();
         if (this.ppRichText) {
@@ -100,6 +99,19 @@ export default class PrivacyPolicyViewer extends LightningElement {
         }
         var myElement = this.template.querySelector('[data-id="text"]');
         myElement.innerHTML = this.ppRichText;
+        if (this.lanCode != null) {
+            if (rtlLanguages.includes(this.lanCode)) {
+                this.isRTL = true;
+            }
+        }
+        if (this.isRTL) {
+            this.outerContainerClass = 'outerContainerRTL';
+            this.bodyWrapper = 'bodyWrapperRTL';
+            this.wrapCss = 'padding-right: 0px;';
+            this.logoClass = 'logoRTL';
+            this.headerAndLogoClass = 'headerAndLogoRTL';
+            this.ppHeaderClass = 'ppHeaderRTL';
+        }
         if (formFactor != 'Large') {
             this.frmFactor = true;
             this.headerAndLogoClass = 'headerAndLogo';
@@ -112,19 +124,26 @@ export default class PrivacyPolicyViewer extends LightningElement {
     connectedCallback() {
         loadScript(this, RR_COMMUNITY_JS)
             .then(() => {
+                this.lanCode = communityService.getUrlParameter('lanCode')
+                    ? communityService.getUrlParameter('lanCode')
+                    : null;
+                if (rtlLanguages.includes(communityService.getLanguage())) {
+                    this.isRTL = true;
+                }
+                console.log('lang: ' + communityService.getLanguage());
                 let userDefalutTC = communityService.getUrlParameter('default') ? true : false;
                 let HasIQVIAStudiesPI = communityService.getHasIQVIAStudiesPI() ? true : false;
                 let ppGetter = getPrivacyPolicy({
-                        code: 'PrivacyPolicy',
-                        languageCode: communityService.getUrlParameter('language'),
-                        useDefaultCommunity: HasIQVIAStudiesPI && userDefalutTC //this.defaultCommunityBoolean
-                    })
-                    .then((result) => {
+                    code: 'PrivacyPolicy',
+                    languageCode: communityService.getUrlParameter('language'),
+                    useDefaultCommunity: HasIQVIAStudiesPI && userDefalutTC //this.defaultCommunityBoolean
+                })
+                    .then(result => {
                         let tcData = JSON.parse(result);
                         this.tcId = tcData.tc.Id;
                         this.ppRichText = tcData.tc.T_C_Text__c;
                         this.lastUpdated = tcData.tc.Last_Updated_on__c;
-                        var lists = tcData.tc.Policy_Headers__c.split("\r\n");
+                        var lists = tcData.tc.Policy_Headers__c.split('\r\n');
                         var psrsList;
                         var psrsList1;
 
@@ -135,13 +154,12 @@ export default class PrivacyPolicyViewer extends LightningElement {
                                 sno: index + 1
                             };
                         });
-
                     })
-                    .catch((error) => {
+                    .catch(error => {
                         console.log(JSON.stringify(error));
                     });
             })
-            .catch((error) => {
+            .catch(error => {
                 this.dispatchEvent(
                     new ShowToastEvent({
                         title: 'Error loading RR_COMMUNITY_JS',
