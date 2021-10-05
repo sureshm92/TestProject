95126f078e830e11ba7390310fef52de530d9fc7
Pijush Kar <pijush.kar@quintiles.com>
2021-10-05 17:59:42 +0530

    script update

--- create-scratch-org.sh
+++ create-scratch-org.sh
@@ -3,11 +3,31 @@
 echo "Clean up previous scratch org"
 sfdx force:org:delete -p
 
-# Rename community for deployment (IQVIA_Referral_Hub_C -> IQVIA_Referral_Hub1)
-echo "Change IQVIA_Referral_Hub_C -> IQVIA_Referral_Hub1"
+echo "Modifying files for scratch org"
+echo "Step 1 - Move current files to scratch-org-files/original-files"
 sed -i 's/IQVIA_Referral_Hub_C/IQVIA_Referral_Hub1/g' 'force-app/communities/community-iqvia/networks/IQVIA Referral Hub.network-meta.xml'
-mv force-app/communities/community-iqvia/experiences/IQVIA_Referral_Hub_C.site-meta.xml force-app/communities/community-iqvia/experiences/IQVIA_Referral_Hub1.site-meta.xml
-mv force-app/communities/community-iqvia/experiences/IQVIA_Referral_Hub_C force-app/communities/community-iqvia/experiences/IQVIA_Referral_Hub1
+mv ./force-app/communities/community-iqvia/experiences ./scratch-org-files/original-files
+mv ./force-app/communities/community-gsk/sites/GSK_Community.site-meta.xml ./scratch-org-files/original-files
+mv ./force-app/communities/community-iqvia/sites/GSK_Community.site-meta.xml ./scratch-org-files/original-files
+mv ./force-app/communities/community-janssen/sites/GSK_Community.site-meta.xml ./scratch-org-files/original-files
+mv ./force-app/main/default/profiles/Business Administrator.profile-meta.xml ./scratch-org-files/original-files
+mv ./force-app/main/default/profiles/Business Lead.profile-meta.xml ./scratch-org-files/original-files
+mv ./force-app/main/default/profiles/IQVIA Customer Community Plus Login User.profile-meta.xml ./scratch-org-files/original-files
+mv ./force-app/main/default/profiles/IQVIA SPNL2.profile-meta.xml ./scratch-org-files/original-files
+mv ./force-app/main/default/profiles/Product Owner.profile-meta.xml ./scratch-org-files/original-files
+mv ./force-app/main/default/workflows/Participant_Enrollment__c.workflow-meta.xml ./scratch-org-files/original-files
+
+echo "Step 2 - Move backup files the project directory"
+mv ./scratch-org-files/experiences ./force-app/communities/community-iqvia
+mv ./scratch-org-files/GSK_Community.site-meta.xml ./force-app/communities/community-gsk/sites
+mv ./scratch-org-files/IQVIA_Referral_Hub.site-meta.xml ./force-app/communities/community-iqvia/sites
+mv ./scratch-org-files/Janssen_Community.site-meta.xml ./force-app/communities/community-janssen/sites
+mv ./scratch-org-files/Business Administrator.profile-meta.xml ./force-app/main/default/profiles
+mv ./scratch-org-files/Business Lead.profile-meta.xml ./force-app/main/default/profiles
+mv ./scratch-org-files/IQVIA Customer Community Plus Login User.profile-meta.xml ./force-app/main/default/profiles
+mv ./scratch-org-files/IQVIA SPNL2.profile-meta.xml ./force-app/main/default/profiles
+mv ./scratch-org-files/Product Owner.profile-meta.xml ./force-app/main/default/profiles
+mv ./scratch-org-files/Participant_Enrollment__c.workflow-meta.xml ./force-app/main/default/workflows
 
 echo "Move communities"
 mv ./force-app/communities ./
@@ -73,9 +93,3 @@ else
     
     echo "Push not completed properly, check logs and try again"
 fi
-
-# Reset the community name (from: IQVIA_Referral_Hub1 -> to: IQVIA_Referral_Hub_C)
-echo "Change IQVIA_Referral_Hub1 -> IQVIA_Referral_Hub_C"
-sed -i 's/IQVIA_Referral_Hub1/IQVIA_Referral_Hub_C/g' 'force-app/communities/community-iqvia/networks/IQVIA Referral Hub.network-meta.xml'
-mv force-app/communities/community-iqvia/experiences/IQVIA_Referral_Hub1.site-meta.xml force-app/communities/community-iqvia/experiences/IQVIA_Referral_Hub_C.site-meta.xml
-mv force-app/communities/community-iqvia/experiences/IQVIA_Referral_Hub1 force-app/communities/community-iqvia/experiences/IQVIA_Referral_Hub_C
