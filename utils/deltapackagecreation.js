476a9213846c7cfc0c4f3b259ee12f8b7e3f157c
Keyur Shah <keyur.shah3@quintiles.com>
2021-11-11 16:16:25 +0530

    Delta package Changes

--- utils/deltapackagecreation.js
+++ utils/deltapackagecreation.js
@@ -9,25 +9,32 @@ async function createDeltaPackage(metadatRecMap) {
         },
         Aura: {
             name: 'AuraDefinitionBundle'
+        },
+        ApexPage: {
+            name: 'ApexPage'
+        },
+        LightningComponentBundle: {
+            name: 'LightningComponentBundle'
         }
     };
     //creating package.xml in DeltaPackage
-    componentMap = await createPackageXML(metadatRecMap,componentMap);
+    componentMap = await createPackageXML(metadatRecMap, componentMap);
     //getting files and putting it in DeltaPackage
-    getAllFiles(path.join(__dirname, '../', 'force-app'),componentMap);
+    getAllFiles(path.join(__dirname, '../', 'force-app'), componentMap);
 }
 
-function createPackageXML(metadatRecMap,componentMap) {
+function createPackageXML(metadatRecMap, componentMap) {
     const root = create({ version: '1.0', encoding: 'UTF-8' });
     let packageElement = root.ele('Package', { xmlns: 'http://soap.sforce.com/2006/04/metadata' });
-    let MetadataKey = Object.keys(metadatRecMap)
+    packageElement.ele('version').txt(process.env.XML_VERSION).up();
+    let MetadataKey = Object.keys(metadatRecMap);
     for (let key of MetadataKey) {
         componentMap[key].metaData = metadatRecMap[key];
         processComponentMap(path.join(__dirname, '../'), componentMap, packageElement, key);
     }
     packageElement.up();
     const xml = root.end({ prettyPrint: true });
-    let fileName = path.join(__dirname, '../', 'DeltaPackage', '/', 'package.xml')
+    let fileName = path.join(__dirname, '../', 'DeltaPackage', '/', 'package.xml');
     fs.writeFile(fileName, xml.toString(), function (err) {
         if (err) throw err;
         console.log('Saved!');
@@ -37,7 +44,11 @@ function createPackageXML(metadatRecMap,componentMap) {
 
 function processComponentMap(dirPath, componentMap, packageElement, key) {
     if (key == 'ApexClass') {
-        addElementsToPackage(packageElement, componentMap.ApexClass.metaData, componentMap.ApexClass.name);
+        addElementsToPackage(
+            packageElement,
+            componentMap.ApexClass.metaData,
+            componentMap.ApexClass.name
+        );
         let classArray = [];
         for (let className of componentMap.ApexClass.metaData) {
             className = className + '.cls';
@@ -46,11 +57,43 @@ function processComponentMap(dirPath, componentMap, packageElement, key) {
             classArray.push(classMetaFileName);
         }
         componentMap.ApexClass.classArray = classArray;
-        componentMap.ApexClass.classDestinationPath = path.join(dirPath, '/', 'DeltaPackage/classes');
+        componentMap.ApexClass.classDestinationPath = path.join(
+            dirPath,
+            '/',
+            'DeltaPackage/classes'
+        );
+    }
+    if (key == 'ApexPage') {
+        addElementsToPackage(
+            packageElement,
+            componentMap.ApexPage.metaData,
+            componentMap.ApexPage.name
+        );
+        let pageArray = [];
+        for (let pageName of componentMap.ApexPage.metaData) {
+            pageName = pageName + '.page';
+            pageMetaFileName = pageName + '-meta.xml';
+            pageArray.push(pageName);
+            pageArray.push(pageMetaFileName);
+        }
+        componentMap.ApexPage.pageArray = pageArray;
+        componentMap.ApexPage.pageDestinationPath = path.join(dirPath, '/', 'DeltaPackage/pages');
     }
     if (key == 'Aura') {
         addElementsToPackage(packageElement, componentMap.Aura.metaData, componentMap.Aura.name);
         componentMap.Aura.auraDestinationPath = path.join(dirPath, '/', 'DeltaPackage/aura');
+    }
+    if (key == 'LightningComponentBundle') {
+        addElementsToPackage(
+            packageElement,
+            componentMap.LightningComponentBundle.metaData,
+            componentMap.LightningComponentBundle.name
+        );
+        componentMap.LightningComponentBundle.lwcDestinationPath = path.join(
+            dirPath,
+            '/',
+            'DeltaPackage/lwc'
+        );
     }
 }
 
@@ -64,16 +107,23 @@ function addElementsToPackage(packageElement, elements, elementType) {
     typesElements.up();
 }
 
-function getAllFiles(dirPath,componentMap) {
+function getAllFiles(dirPath, componentMap) {
     files = fs.readdirSync(dirPath);
     let aura;
     let classes;
-
-    if (componentMap.ApexClass.metaData!=undefined) {
+    let pages;
+    let lwcs;
+    if (componentMap.ApexClass.metaData != undefined) {
         classes = componentMap['ApexClass'].classArray;
     }
-    if (componentMap.Aura.metaData!=undefined) {
+    if (componentMap.ApexPage.metaData != undefined) {
+        pages = componentMap['ApexPage'].pageArray;
+    }
+    if (componentMap.Aura.metaData != undefined) {
         aura = componentMap['Aura'].metaData;
+    }
+    if (componentMap.LightningComponentBundle.metaData != undefined) {
+        lwcs = componentMap['LightningComponentBundle'].metaData;
     }
     files.forEach(function (file) {
         if (fs.statSync(dirPath + '/' + file).isDirectory()) {
@@ -86,8 +136,19 @@ function getAllFiles(dirPath,componentMap) {
                     path.join(componentMap.Aura.auraDestinationPath + '/' + file),
                     true
                 );
+            }
+            if (lwcs && lwcs.includes(file)) {
+                checkAndCreateDirecotry(componentMap.LightningComponentBundle.lwcDestinationPath);
+                console.log(path.join(dirPath + '/' + file));
+                copyFileOrDirectory(
+                    path.join(dirPath + '/' + file),
+                    path.join(
+                        componentMap.LightningComponentBundle.lwcDestinationPath + '/' + file
+                    ),
+                    true
+                );
             } else {
-                getAllFiles(dirPath + '/' + file,componentMap);
+                getAllFiles(dirPath + '/' + file, componentMap);
             }
         } else {
             if (classes && classes.includes(file)) {
@@ -96,6 +157,15 @@ function getAllFiles(dirPath,componentMap) {
                 copyFileOrDirectory(
                     path.join(dirPath + '/' + file),
                     path.join(componentMap.ApexClass.classDestinationPath + '/' + file),
+                    false
+                );
+            }
+            if (pages && pages.includes(file)) {
+                checkAndCreateDirecotry(componentMap.ApexPage.pageDestinationPath);
+                //console.log(path.join(dirPath, '/', file));
+                copyFileOrDirectory(
+                    path.join(dirPath + '/' + file),
+                    path.join(componentMap.ApexPage.pageDestinationPath + '/' + file),
                     false
                 );
             }
@@ -135,9 +205,6 @@ function copyFolderSync(from, to) {
         }
     });
 }
-
-
-
 
 module.exports = {
     init: createDeltaPackage
