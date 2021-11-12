9a5825a47f9ba28c42aab2583fe318ba6264e22a
velu <veluman.vijayaragavan@quitiles.com>
2021-11-12 15:30:30 +0530

    Toast Label Change

--- force-app/main/default/lwc/rP_BulkProfileSectionPage/rP_BulkProfileSectionPage.js
+++ force-app/main/default/lwc/rP_BulkProfileSectionPage/rP_BulkProfileSectionPage.js
@@ -163,7 +163,7 @@ export default class RP_BulkProfileSectionPage extends LightningElement {
        if(communityService.isMobileSDK()){
         const evt = new ShowToastEvent({
             title: '',
-            message: 'This feature supports only in web browser experience',
+            message: 'Opening this link is only supported using the web browser experience',
             variant: 'info',
             mode: 'dismissable'
         });
