8583f7753b0e6c7dcc75fb6aa920f3a8f4650f4b
Vijaya Priya <vijaya.priya@quintiles.com>
2021-12-02 10:23:13 +0000

    Update run-tests.sh

--- scripts/ci/run-tests.sh
+++ scripts/ci/run-tests.sh
@@ -7,4 +7,4 @@ sfdx force:auth:sfdxurl:store -f authFile -a TargetOrg -d
 
 # Run tests
 #sfdx force:apex:test:run -u TargetOrg --testlevel=RunLocalTests -w 60
-sfdx force:apex:test:run -u TargetOrg -w 350 --resultformat json --outputdir $CI_PROJECT_DIR/tests/apex/ --codecoverage --testlevel RunLocalTests
+sfdx force:apex:test:run -u TargetOrg -w 350 --resultformat json --outputdir $CI_PROJECT_DIR/results/ --codecoverage --testlevel RunLocalTests
