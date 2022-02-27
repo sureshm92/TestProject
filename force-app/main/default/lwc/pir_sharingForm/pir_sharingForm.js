61d809f6707fb8c99fa6153a1e0ecad2104a8e46
Rakesh <rakesh.rangu@iqvia.com>
2022-02-27 23:32:13 +0530

    RH-569--Sharing Options

---
+++ force-app/main/default/lwc/pir_sharingForm/pir_sharingForm.js
@@ -0,0 +1,114 @@
+import { api, LightningElement } from 'lwc';
+
+export default class Pir_sharingOptionForm extends LightningElement {
+    @api addDelegateList;
+    @api addHCPDelegateList;
+    @api isAddDelegates;
+    @api isHCPAddDelegates;
+    @api yob;
+    @api existingDelegates;
+    @api peDetails;
+    sharingObject;
+    @api participantObject;
+    @api targetObject;
+    @api isHCPDelegate;
+    buttonLabel;
+
+    connectedCallback(){
+        if(this.targetObject === 'delegate') {
+            this.buttonLabel = 'Add Delegate';
+        } else {
+            this.buttonLabel = 'Add Provider';
+        }
+        this.updateExistingDelegates();
+    }
+
+    @api
+    updateExistingDelegates() {
+        if(this.targetObject == 'delegate') {
+            if(this.addDelegateList) {    
+                let mergedList = this.addDelegateList;           
+                for(let i=0; i<mergedList;i++) {
+                    let obj = {"sObjectType": 'Object'};
+                    let mergedObj = { ...mergedList[i], ...obj };                    
+                    mergedList[i] = mergedObj;
+                }
+                this.addDelegateList = mergedList;
+                this.isAddDelegates = false;
+                this.isHCPDelegate = true;
+                this.isAddDelegates = true;
+                this.isHCPDelegate = false;
+            }
+        }
+        if(this.targetObject == 'hcp') {
+            if(this.addHCPDelegateList) {
+                let mergedList = this.addHCPDelegateList;
+                for(let i=0; i<mergedList.length;i++) {
+                    let obj = {"sObjectType": 'Healthcare_Provider__c'};
+                    let mergedObj = { ...mergedList[i], ...obj };
+                    mergedList[i] = mergedObj;
+                }
+                this.addHCPDelegateList = mergedList;
+                this.isHCPDelegate = false;
+                this.isHCPAddDelegates = false;
+                this.isHCPDelegate = true;
+                this.isHCPAddDelegates = true;
+            }
+        }
+        console.log('this.addDelegateList:'+JSON.stringify(this.addDelegateList));
+    }
+
+    @api
+    updateExistingHCPDelegates() {
+
+
+    }
+
+    addDelegateFormFields() {
+        if(this.targetObject == 'delegate') {
+            this.isAddDelegates = false;
+            this.isHCPDelegate = true;
+            let delegateObj = {sObjectType:'Object'};
+            
+            if(!this.addDelegateList) {
+                this.addDelegateList= [];
+                this.addDelegateList.push(delegateObj)
+            } else {
+                let delegateList = [];
+                delegateList.push(delegateObj);
+                this.addDelegateList = [...this.addDelegateList, ...delegateList];
+            }
+            //this.addDelegateList = delegateList;
+            if( this.addDelegateList.length >= 0) {            
+                this.isAddDelegates = true;     
+                this.isHCPDelegate = false;  
+            }
+        } else {
+            this.isHCPAddDelegates = false;
+            this.isHCPDelegate = false;
+            let hcpDelegateObj = {sObjectType:'Healthcare_Provider__c'};
+
+            if(!this.addHCPDelegateList) {
+                this.addHCPDelegateList= [];
+                this.addHCPDelegateList.push(hcpDelegateObj)
+            } else {
+                let hcpDelegateList = [];
+                hcpDelegateList.push(hcpDelegateObj);
+                this.addHCPDelegateList = [...this.addHCPDelegateList, ...hcpDelegateList];
+            }
+
+            if( this.addHCPDelegateList.length >= 0) {            
+                this.isHCPAddDelegates = true;   
+                this.isHCPDelegate = true;     
+            }
+        }
+    }
+    
+    @api resetDelegateList() {
+        this.isAddDelegates = false;
+        this.addDelegateList = [];
+        this.isHCPAddDelegates = false;
+        this.addHCPDelegateList = [];
+    }    
+    
+}
---
+++ force-app/main/default/lwc/pir_sharingOption/pir_sharingOption.js
@@ -0,0 +1,121 @@
+import { LightningElement, api, wire } from "lwc";
+import getInitData from '@salesforce/apex/ReferHealthcareProviderRemote.getInitData';
+import getParticipantData from '@salesforce/apex/PIR_SharingOptionsController.fetchParticipantData';
+
+export default class Pir_sharingOption extends LightningElement {
+    
+    value = [];
+    datafetchsuccess = false;
+    get options() {
+        return [
+            { label: '2022', value: '2022' },
+            { label: '2021', value: '2021' },
+            { label: '2020', value: '2020' },
+            { label: '2019', value: '2019' },
+          
+        ];
+    }
+    handleChange(e) {
+        this.value = e.detail.value;
+    }
+    @api 
+    selectedPE;
+    healthcareProviders = [];
+    delegates = [];
+    yob = [];
+    loading = false;
+    participantObject;
+    isHCPDelegates;
+    isAddDelegates;
+    
+
+    connectedCallback(){
+        console.log('>>connected call back called>>'+this.selectedPE);
+        this.fetchInitialDetails();
+
+    }
+
+    @api
+    fetchInitialDetails() {  
+        console.log('>>fecth details called>>'); 
+       
+        this.getParticipantDetails();
+        this.getInitialData();
+       
+        //this.loading = false;
+        this.template.querySelector("c-pir_sharing-form").resetDelegateList();
+        this.template.querySelector("c-pir_sharing-form").isAddDelegates = false;        
+        this.template.querySelector("c-pir_sharing-form").peDetails = this.selectedPE;
+        loading = false;
+        
+    }
+
+    getParticipantDetails() {
+        if(!this.loading) {
+            this.loading = true;
+        }
+        getParticipantData({perId:this.selectedPE.id})
+            .then((result) => {
+               
+                console.log('ParticipantData:'+JSON.stringify(result));
+               // this.template.querySelector("c-pir_sharing-form").participantObject = result;
+                this.participantObject = result;
+                this.loading = false;
+                
+            })
+            .catch((error) => {
+                //this.error = error;
+                console.log(error);
+                this.loading = false;
+            });
+    }
+
+    getInitialData() {
+        console.log('>>init detail called>>');
+        if(!this.loading) {
+            this.loading = true;
+        }
+        getInitData({ peId: this.selectedPE.id, participantId: this.selectedPE.participantId })
+        .then((result) => {
+           
+            console.log('Result:'+JSON.stringify(result));
+            let hcp = result.healthcareProviders;
+            for (let i = 0; i < hcp.length; i++) {
+                hcp[i].sObjectType = 'Healthcare_Provider__c';
+            }
+            let del = result.listWrapp;
+            console.log(JSON.stringify(result.listWrapp));
+            for (let i = 0; i < del.length; i++) {
+                del[i].sObjectType = 'Object';
+            }
+            //if(del.length > 0) {
+                this.delegates = del;
+                this.isAddDelegates = true;
+            //}
+            //if(hcp.length > 0) {
+                this.healthcareProviders = hcp;
+                this.ishcpAddDelegates = true;
+            
+
+            //this.template.querySelector("c-pir_sharing-form").updateExistingDelegates();
+            //this.template.querySelector("c-pir_sharing-form").updateExistingHCPDelegates();
+
+            this.loading = false;
+           
+        })
+        .catch((error) => {
+            //this.error = error;
+            console.log(error);
+            this.loading = false;
+            //this.showErrorToast(JSON.stringify(error.body.message));
+        });
+    }
+
+    refreshDelegates() {
+        this.fetchInitialDetails();
+
+    }
+    
+
+
+}
