1e3004b2184cfcd6b1d6aed89c859644157c8440
Rakesh <rakesh.rangu@iqvia.com>
2022-02-28 19:54:39 +0530

    RH-569--Issue Fix

--- force-app/main/default/lwc/pir_duplicateDelegateMessage/pir_duplicateDelegateMessage.js
+++ force-app/main/default/lwc/pir_duplicateDelegateMessage/pir_duplicateDelegateMessage.js
@@ -1,15 +1,18 @@
 import { LightningElement, api } from 'lwc';
 import Duplicatelabel from '@salesforce/label/c.Duplicate_Delegate_Message';
+import myResource from '@salesforce/resourceUrl/icon_statusAlertOrange';
 
 export default class Pir_duplicateDelegateMessage extends LightningElement {
     @api duplicateInfo;
     duplicateMessage;
+    imgSrc;
     label = {
         Duplicatelabel
     }
 
     connectedCallback() {
-        if(this.duplicateInfo.isDuplicateDelegate) {            
+        if(this.duplicateInfo.isDuplicateDelegate) {       
+            this.imgSrc = myResource;     
             let msg = this.label.Duplicatelabel;
             msg = msg.replace(new RegExp('##firstName', 'g'), this.duplicateInfo.firstName);
             msg = msg.replace('##lastName', this.duplicateInfo.lastName);
