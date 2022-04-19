4e3ef3869ac5837eeedf24e5c64738fec97a4d22
u1120692 <jayashree.j@iqvia.com>
2022-04-19 14:54:33 +0530

    PEH-4892- Post Login Theme Layout

--- force-app/main/community-template/aura/CommunityNavigation/CommunityNavigationController.js
+++ force-app/main/community-template/aura/CommunityNavigation/CommunityNavigationController.js
@@ -1,5 +1,6 @@
 ({
     doInit: function(cmp,event) {
+        cmp.set('v.communityName', communityService.getCurrentCommunityName());
         setTimeout(function(){ 
             if('my-referrals'==cmp.get("v.currentPage")||'my-patients'==cmp.get("v.currentPage")){
                 document.getElementsByClassName("active")[0].click();
