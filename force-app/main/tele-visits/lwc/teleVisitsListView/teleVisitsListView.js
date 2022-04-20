9c1273eae7404724774fd963372ec064a9d450cb
Pijush Kar <pijush.kar@quintiles.com>
2022-04-20 19:33:30 +0530

    pagination

--- force-app/main/tele-visits/lwc/teleVisitsListView/teleVisitsListView.js
+++ force-app/main/tele-visits/lwc/teleVisitsListView/teleVisitsListView.js
@@ -21,9 +21,49 @@ export default class TeleVisitsListView extends LightningElement {
         { value: 'Past', label: 'Past' },
         { value: 'Cancelled', label: 'Cancelled' }
     ];
-    teleVisits = [{}];
+    teleVisits = [
+        {
+            id: '0',
+            visitName: 'Visit 1',
+            visitDate: '2022-04-18T11:16:47.000+0000',
+            visitDuration: '30 minutes',
+            visitAttendees: 'Mike Tyson (PI)'
+        },
+        {
+            id: '1',
+            visitName: 'Visit 2',
+            visitDate: '2022-04-18T11:16:47.000+0000',
+            visitDuration: '30 minutes',
+            visitAttendees: 'Mike Tyson (PI)'
+        },
+        {
+            id: '2',
+            visitName: 'Visit 3',
+            visitDate: '2022-04-18T11:16:47.000+0000',
+            visitDuration: '30 minutes',
+            visitAttendees: 'Mike Tyson (PI)'
+        },
+        {
+            id: '3',
+            visitName: 'Visit 4',
+            visitDate: '2022-04-18T11:16:47.000+0000',
+            visitDuration: '30 minutes',
+            visitAttendees: 'Mike Tyson (PI)'
+        },
+        {
+            id: '4',
+            visitName: 'Visit 5',
+            visitDate: '2022-04-18T11:16:47.000+0000',
+            visitDuration: '30 minutes',
+            visitAttendees: 'Mike Tyson (PI)'
+        }
+    ];
     isInitialized = true;
     timeZone = TIMEZONE;
+    entriesOnPage = 4;
+    pageNumber = 1;
+    allRecordsCount = 5;
+    teleVisitsToDisplay = [];
 
     /*connectedCallback() {
         loadScript(this, RR_COMMUNITY_JS)
@@ -53,5 +93,9 @@ export default class TeleVisitsListView extends LightningElement {
     }
     get filterLabel() {
         return this.labels.FILTER_LABEL;
+    }
+    handlePaginatorChanges(event) {
+        this.teleVisitsToDisplay = event.detail;
+        //this.rowNumberOffset = this.teleVisitsToDisplay[0].rowNumber - 1;
     }
 }
