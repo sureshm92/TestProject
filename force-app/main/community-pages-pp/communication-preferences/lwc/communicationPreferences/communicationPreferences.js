156e1fdd9d3e8bb60440d1dd7b6c786de3deda72
krishna kumar mahto <krishnakumar.mahto@quintiles.com>
2022-06-15 14:46:39 +0530

    PEH-5682

--- force-app/main/community-pages-pp/communication-preferences/lwc/communicationPreferences/communicationPreferences.js
+++ force-app/main/community-pages-pp/communication-preferences/lwc/communicationPreferences/communicationPreferences.js
@@ -1,6 +1,7 @@
 import { LightningElement, track, api, wire} from 'lwc';
-import { ShowToastEvent } from 'lightning/platformShowToastEvent'; // TOBE: implemented through communityService
-// TO DO : Replace all hardcoded text from html file with custom label's (cross check if already labels exist for ex. save changes is common used in multiple front end components)
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { NavigationMixin } from 'lightning/navigation';
+import rr_community_icons from '@salesforce/resourceUrl/rr_community_icons';
 import PP_Communication_Pref from '@salesforce/label/c.PP_Communication_Pref';
 import PP_Communication_Pref_Study from '@salesforce/label/c.PP_Communication_Pref_Study';
 import PP_Communication_Pref_Study_consent from '@salesforce/label/c.PP_Communication_Pref_Study_consent';
@@ -10,17 +11,20 @@ import PP_Communication_Pref_SMS from '@salesforce/label/c.PP_Communication_P
 import PP_Communication_Pref_Dmail from '@salesforce/label/c.PP_Communication_Pref_Dmail';
 import PP_Communication_Pref_All from '@salesforce/label/c.PP_Communication_Pref_All';
 import PP_Communication_Pref_Outreach from '@salesforce/label/c.PP_Communication_Pref_Outreach';
-import PP_Communication_Pref_Outreach_consentA from '@salesforce/label/c.PP_Communication_Pref_Outreach_consentA';
-import PP_Communication_Pref_Outreach_consentB from '@salesforce/label/c.PP_Communication_Pref_Outreach_consentB';
-import PP_Communication_Pref_Outreach_consentC from '@salesforce/label/c.PP_Communication_Pref_Outreach_consentC';
+import PP_Communication_Pref_Outreach_consent from '@salesforce/label/c.PP_Communication_Pref_Outreach_consent';
 import PP_Communication_Pref_savechanges from '@salesforce/label/c.PP_Communication_Pref_savechanges';
 //END TO DO
 
 import getStudiesConsent from "@salesforce/apex/PreferenceManagementController.getStudiesConsent";
 import getOutreachConsent from "@salesforce/apex/PreferenceManagementController.getOutreachConsent";
 import saveConsent from "@salesforce/apex/PreferenceManagementController.saveConsent";
+import getisRTL from '@salesforce/apex/PreferenceManagementController.getIsRTL';
 
-export default class CommunicationPreferences extends LightningElement {
+import { loadScript } from 'lightning/platformResourceLoader';
+import rrCommunity from '@salesforce/resourceUrl/rr_community_js';
+
+
+export default class CommunicationPreferences extends NavigationMixin(LightningElement) {
 
   @api
   label = {
@@ -33,9 +37,7 @@ export default class CommunicationPreferences extends LightningElement {
     PP_Communication_Pref_Dmail,
     PP_Communication_Pref_All,
     PP_Communication_Pref_Outreach,
-    PP_Communication_Pref_Outreach_consentA,
-    PP_Communication_Pref_Outreach_consentB,
-    PP_Communication_Pref_Outreach_consentC,
+    PP_Communication_Pref_Outreach_consent,
     PP_Communication_Pref_savechanges
   };
 
@@ -43,16 +45,28 @@ export default class CommunicationPreferences extends LightningElement {
     @track outReachDetails= [];
 
     spinner = false;
-    autoOptOutStudies = [];
-    isDesktop = false;
+    loaded = false;
+    isDesktop = true;
+    isPrivacyPolicy = false;
 
+    debugger;
+    //@track phoneSvg = rr_community_icons +'/'+'logo.svg';
+    phoneSvg = rr_community_icons +'/'+'com-phone.svg'+'#'+'com-phone';
+    emailSvg = rr_community_icons +'/'+'com-email.svg'+'#'+'com-email';
+    smsSvg = rr_community_icons +'/'+'com-sms.svg'+'#'+'com-sms';
+    dmailSvg = rr_community_icons +'/'+'com-dmail.svg'+'#'+'com-dmail';
+    allSvg = rr_community_icons +'/'+'com-all.svg'+'#'+'com-all';
+
+    
     @wire(getStudiesConsent)
     studyConsent({ error, data }) {
         if (data) {
+            debugger;
             this.studyDetails = JSON.parse(data);
         } else if (error) {
             console.log(JSON.stringify(error));
         }
+        this.resetSpinner();
     }
     @wire(getOutreachConsent)
     outReachConsent({ error, data }) {
@@ -61,10 +75,44 @@ export default class CommunicationPreferences extends LightningElement {
         } else if (error) {
             console.log(JSON.stringify(error));
         }
+        this.resetSpinner();
     }
 
     connectedCallback(){
-      // TO DO: get init data from Controller
+      this.resetSpinner()
+      if (!this.loaded) {
+        loadScript(this, rrCommunity).then(() => {
+            if (communityService.isMobileSDK()) {
+                debugger;
+                this.isDesktop = false;
+            }
+        });
+        getisRTL()
+            .then((data) => {
+                debugger;
+                this.isRTL = data;
+            })
+            .catch(function (error) {
+                console.error('Error: ' + JSON.stringify(error));
+            });
+        }
+    }
+
+    resetSpinner(){
+      if(this.studyDetails.length == 0 || this.outReachDetails.length ==0){
+        this.spinner = true;
+      }
+      else{
+        this.spinner = false;
+      }
+    }
+
+    get cardRTL() {
+      return this.isRTL ? 'cardRTL' : '';
+    }
+
+    get borderStyle(){
+      return this.isRTL ? 'study-paramters border-left' : 'study-paramters border-right'
     }
 
     renderedCallback(){
@@ -78,61 +126,32 @@ export default class CommunicationPreferences extends LightningElement {
       this.updateStudyData(event.target.label, event.target.checked, event.target.name, event.target.getAttribute("data-sponsor"));  
     }
 
-    updateStudyData(label, value, studyId, sponsorId, perId){
+    updateStudyData(label, value, studyId, sponsorId){
       debugger;
       if(label == "All"){
-          let autoOptOutStudies = [];
           this.studyDetails.forEach(function (study) {
               debugger;             
-              // Opt out SMS at sponser level
-              if(study.sponsor_id == sponsorId && !value){                  
-                  study.sms = value;
-                  if(study.study_id != studyId){
-                    autoOptOutStudies.push(study.study_name);
-                  }
-              }  
-              // opt IN SMS at Study Level
-              if(study.study_id == studyId && value){                  
-                  study.sms = value;
-              } 
-              // opt IN Phone, Email, Direct Email at Study Level 
+              // opt IN/ opt Out Phone, Email, Direct Email, SMS at Study Level 
               if(study.study_id == studyId){
-                study.phone = study.email = study.dmail = value;
+                study.phone = study.email = study.dmail = study.sms = value;
               }
           });
-          this.autoOptOutStudies = autoOptOutStudies;
-          // update All Flag
-          this.updateALLFlag();
       }
       else{
         this.studyDetails.forEach(function (study) {
-          // If not clicked on All - Individual operaton 
-          
-          if(label == "SMS"){
-            console.log("SMS Individual Handler");
-            // Opt out SMS at sponser level
-            if(study.sponsor_id == sponsorId && !value){                  
-              study.sms = value;
-              if(study.study_id != studyId){
-                this.autoOptOutStudies.push(study.study_name);
-              }
-            }  
-            // opt IN SMS at Study Level
-            if(study.study_id == studyId && value){                  
-                study.sms = value;
-            } 
-          }   
-          else{
+          // If not clicked on All - Individual operaton
           (study.study_id == studyId && label == "Phone" ? 
               study.phone = value : 
               (study.study_id == studyId && label == "Email" ? 
                 study.email = value:
+                (study.study_id == studyId && label == "SMS" ? 
+                study.sms = value:
                  (study.study_id == studyId && label == "DMail" ?  
-                   study.dmail = value: "")));
-          }    
+                   study.dmail = value: ""))));        
         });
-        this.updateALLFlag();
-      }      
+      } 
+      this.updateALLFlag();
+      this.doSaveCommunicationPref();    
     }
 
     // Helper Method : To Update ALL flag comparing phone, email, sms and direct email at study as well as sponsor level
@@ -145,32 +164,28 @@ export default class CommunicationPreferences extends LightningElement {
     // Save Communication Pref data
     doSaveCommunicationPref(){
       debugger;
-      let response = true;
-
-      if(this.autoOptOutStudies.length > 0){
-        let relatedStudies = this.autoOptOutStudies.toString();
-        let message = `You will be automatically opted out from <b>${relatedStudies}</b> for the same sponsor. \n Are you sure you want to proceed further? `
-        response = confirm(message);
-      }
-      if(response){
-        this.spinner = true;
-        saveConsent({ studyData: this.studyDetails, outReachData: this.outReachDetails })
+      this.spinner = true;
+      saveConsent({ studyData: this.studyDetails, outReachData: this.outReachDetails })
         .then((result) => {
-            console.log("Result of Save");
-            console.log(result);
             setTimeout(() => {
               this.spinner = false;
-              this.showCustomToast("Success", "Record Saved Successfully", "success");
+              // this.showCustomToast("Success", "Record Saved Successfully", "success");
+              communityService.showToast(
+                'success',
+                'success',
+                'Record Saved Successfully...'
+              );
             }, 3000); 
         })
         .catch((error) => {
-          this.showCustomToast("Error", "Failed To save the Record", "error");
+          // this.showCustomToast("Error", "Failed To save the Record", "error");
+          communityService.showToast(
+            'error',
+            'error',
+            'Failed To save the Record...'
+          );
+          this.spinner = false;
         });
-       
-      }
-      else{
-        this.spinner = false;
-      }          
     } 
 
     // Helper Method: Toast Notification
@@ -190,6 +205,7 @@ export default class CommunicationPreferences extends LightningElement {
       this.updateOutReachData(event.target.label, event.target.checked, event.target.name);
 
     }
+
     selectIndividualOptionsOutreach(event){
       this.updateOutReachData(event.target.label, event.target.checked, event.target.name);
     }
@@ -210,7 +226,8 @@ export default class CommunicationPreferences extends LightningElement {
                         break;
         }
       });
-      this.updateALLOutReachFlag()    
+      this.updateALLOutReachFlag();
+      this.doSaveCommunicationPref(); 
     }
 
     updateALLOutReachFlag(){
