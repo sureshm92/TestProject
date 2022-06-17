5a90b0096786e7fc0d2cea128b4b228560361f70
krishna kumar mahto <krishnakumar.mahto@quintiles.com>
2022-06-17 13:34:06 +0530

    PEH-5862  - comment change

--- force-app/main/community-pages-pp/communication-preferences/lwc/communicationPreferences/communicationPreferences.js
+++ force-app/main/community-pages-pp/communication-preferences/lwc/communicationPreferences/communicationPreferences.js
@@ -241,7 +241,7 @@ export default class CommunicationPreferences extends NavigationMixin(LightningE
     }
 		//Return True if Delegate is logged in as self View
 		get isDelegateSelfView() {
-				//TODO: Cover this part as part of PEH-5843 - UI part in Iteration 3.2
+				//TODO: Cover this part as part of PEH-5844 - UI part in Iteration 3.2
 				/*
 				  if (this.participantState === 'ALUMNI' && !this.isDelegate){
 						return true;
