8094795acce2ca025ec68c768d0d9e54d8c43eb2
u1126733 <chavva.anitha@iqvia.com>
2022-07-11 17:07:08 +0530

    PEH-6259

--- force-app/main/community-pages-pp_2/lwc/pageProgramOverviewDetails/pageProgramOverviewDetails.js
+++ force-app/main/community-pages-pp_2/lwc/pageProgramOverviewDetails/pageProgramOverviewDetails.js
@@ -1,4 +1,4 @@
-import { LightningElement ,api} from 'lwc';
+import { LightningElement, api } from 'lwc';
 import getParticipantData from '@salesforce/apex/HomePageParticipantRemote.getInitData';
 import rr_community_icons from '@salesforce/resourceUrl/rr_community_icons';
 
@@ -7,22 +7,20 @@ import mobileTemplate from './mobileTemplate.html';
 import DEVICE from '@salesforce/client/formFactor';
 
 export default class ProgramOverviewDetails extends LightningElement {
-     programname;
-     participantState;
-     clinicaltrailrecrd; 
-     activeTab ='overview';
-     homeSvg = rr_community_icons + '/' + 'icons.svg' + '#' + 'icon-home-pplite-new';
-     ctpAccordionData;
+    programname;
+    participantState;
+    clinicaltrailrecrd;
+    activeTab = 'overview';
+    homeSvg = rr_community_icons + '/' + 'icons.svg' + '#' + 'icon-home-pplite-new';
+    ctpAccordionData;
 
-     desktop = true;
-     tabContent = true;
-     overviewCss = "po-tab-menu active";
-     pcCss = "po-tab-menu";
-    
-    
+    desktop = true;
+    tabContent = true;
+    overviewCss = 'po-tab-menu active';
+    pcCss = 'po-tab-menu';
+
     connectedCallback() {
-
-        DEVICE == 'Small' ? this.desktop = true : this.desktop = false;
+        DEVICE == 'Small' ? (this.desktop = true) : (this.desktop = false);
 
         let ctpaccordionDatalist = [];
         //code
@@ -33,24 +31,33 @@ export default class ProgramOverviewDetails extends LightningElement {
                     if (this.participantState.pe) {
                         if (this.participantState.pe.Clinical_Trial_Profile__r) {
                             this.clinicaltrailrecrd = this.participantState.pe.Clinical_Trial_Profile__r;
-                            if(this.clinicaltrailrecrd){
-                                if(this.clinicaltrailrecrd.Study_Code_Name__c){
-                                    this.programname = 'About '+ this.clinicaltrailrecrd.Study_Code_Name__c
+                            if (this.clinicaltrailrecrd) {
+                                if (this.clinicaltrailrecrd.Study_Code_Name__c) {
+                                    this.programname =
+                                        'About ' + this.clinicaltrailrecrd.Study_Code_Name__c;
                                 }
-                                if(this.clinicaltrailrecrd.Override_Inclusion_Criteria__c){
-                                    ctpaccordionDatalist.push({ id : 0, label :  'Inclusion Criteria' , body : this.clinicaltrailrecrd.Override_Inclusion_Criteria__c});
+                                if (this.clinicaltrailrecrd.Override_Inclusion_Criteria__c) {
+                                    ctpaccordionDatalist.push({
+                                        id: 0,
+                                        label: 'Inclusion Criteria',
+                                        body: this.clinicaltrailrecrd.Override_Inclusion_Criteria__c
+                                    });
                                 }
-                                if(this.clinicaltrailrecrd.Override_Exclusion_Criteria__c){
-                                    ctpaccordionDatalist.push({ id : 1, label :  'Exclusion Criteria' , body : this.clinicaltrailrecrd.Override_Exclusion_Criteria__c});
+                                if (this.clinicaltrailrecrd.Override_Exclusion_Criteria__c) {
+                                    ctpaccordionDatalist.push({
+                                        id: 1,
+                                        label: 'Exclusion Criteria',
+                                        body: this.clinicaltrailrecrd.Override_Exclusion_Criteria__c
+                                    });
                                 }
                                 this.ctpAccordionData = ctpaccordionDatalist;
-                             }
+                            }
                         }
                     }
                 }
             })
             .catch((error) => {
-                console.log('error::' + (error));
+                console.log('error::' + error);
                 this.error = error;
             });
     }
@@ -58,36 +65,34 @@ export default class ProgramOverviewDetails extends LightningElement {
     render() {
         return this.desktop ? desktopTemplate : mobileTemplate;
     }
-    
-    handleHomePage(){
+
+    handleHomePage() {
         communityService.navigateToPage('');
     }
-    // JS functions start 
+    // JS functions start
     handleActive(event) {
         this.activeTab = event.target.value;
     }
 
-    get handleDynamicCSS(){
-        if(this.activeTab){
-            console.log('this.activeTab'+this.activeTab);
-            if(this.activeTab =='overview'){
-            return 'font-size: 50px;';
-            }
-            else if(this.activeTab =='parcrt'){
+    get handleDynamicCSS() {
+        if (this.activeTab) {
+            if (this.activeTab == 'overview') {
+                return 'font-size: 50px;';
+            } else if (this.activeTab == 'parcrt') {
                 return 'font-size: 50px;';
             }
         }
     }
 
-    overViewHandler(){
+    overViewHandler() {
         this.tabContent = true;
-        this.overviewCss = "po-tab-menu active";
-        this.pcCss = "po-tab-menu";
+        this.overviewCss = 'po-tab-menu active';
+        this.pcCss = 'po-tab-menu';
     }
 
-    participationHandler(){
+    participationHandler() {
         this.tabContent = false;
-        this.overviewCss = "po-tab-menu";
-        this.pcCss = "po-tab-menu active";
+        this.overviewCss = 'po-tab-menu';
+        this.pcCss = 'po-tab-menu active';
     }
 }
