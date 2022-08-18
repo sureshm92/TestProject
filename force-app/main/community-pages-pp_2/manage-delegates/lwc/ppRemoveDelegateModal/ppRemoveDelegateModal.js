1eedd099a830ac4c6de96007c068f2fd15bcd577
chavva Anitha <chavva.anitha@iqvia.com>
2022-08-18 16:58:20 +0530

    PEH-5883

--- force-app/main/community-pages-pp_2/manage-delegates/lwc/ppRemoveDelegateModal/ppRemoveDelegateModal.js
+++ force-app/main/community-pages-pp_2/manage-delegates/lwc/ppRemoveDelegateModal/ppRemoveDelegateModal.js
@@ -1,10 +1,32 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,api } from 'lwc';
 
 export default class PpRemoveDelegateModal extends LightningElement {
+    @api callback;
+    @api messtext;
+    @api tittext;
+    @api contact;
+    @api usermode;
+    @api selectedparent;
+    isLoading = false;
+
+    showmodal = false;
+    connectedCallback() {
+        this.showmodal = true;
+
+    }
+
     closeModal() {
         const selectedEvent = new CustomEvent('modalclose', {
             detail: false
         });
         this.dispatchEvent(selectedEvent);
+    }
+    confirmModal(){
+        const selectedEvent = new CustomEvent('confirmmodal', {
+            detail: {contact : this.contact, usermode: this.usermode, selectedparent : this.selectedparent}
+        });
+
+        this.dispatchEvent(selectedEvent);
+        this.showmodal = false;
     }
 }
