7a96eca1bb5b474b58b9ff0e67263edabffcad71
jayashree.j <jayashree.j@iqvia.com>
2022-09-02 21:06:02 +0530

    PEH-7552- Case 3,5

--- force-app/main/community-pages-pp_2/participant-home/lwc/homePageParticipantNew/homePageParticipantNew.js
+++ force-app/main/community-pages-pp_2/participant-home/lwc/homePageParticipantNew/homePageParticipantNew.js
@@ -1,35 +1,63 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 import getParticipantData from '@salesforce/apex/HomePageParticipantRemote.getInitData';
 import DEVICE from '@salesforce/client/formFactor';
 // importing Custom Label
 import PPWELCOME from '@salesforce/label/c.PP_Welcome';
-
+import communityPPTheme from '@salesforce/resourceUrl/Community_CSS_PP_Theme';
+import { loadScript, loadStyle } from 'lightning/platformResourceLoader';
+import RR_COMMUNITY_JS from '@salesforce/resourceUrl/rr_community_js';
 
 export default class HomePageParticipantNew extends LightningElement {
     label = {
         PPWELCOME
     };
-    
+
     participantState;
     clinicalrecord;
     error;
-    userName = "Sarah";
+    userName = 'Sarah';
+    @api currentMode;
+    spinner;
+    isInitialized = false;
 
     desktop = true;
     isDelegateSelfview = false;
-    
-    get showProgramOverview (){
-        return (this.clinicalrecord || this.isDelegateSelfview) ? true : false;
+
+    get showProgramOverview() {
+        return this.clinicalrecord || this.isDelegateSelfview ? true : false;
     }
 
     connectedCallback() {
         DEVICE != 'Small' ? (this.desktop = true) : (this.desktop = false);
+
+        loadScript(this, RR_COMMUNITY_JS)
+            .then(() => {
+                Promise.all([loadStyle(this, communityPPTheme)])
+                    .then(() => {
+                        this.spinner = this.template.querySelector('c-web-spinner');
+                        this.spinner ? this.spinner.show() : '';
+                        this.initializeData();
+                    })
+                    .catch((error) => {
+                        console.log(error.body.message);
+                    });
+            })
+            .catch((error) => {
+                communityService.showToast('', 'error', error.message, 100);
+            });
+    }
+
+    initializeData() {
         getParticipantData()
             .then((result) => {
+                this.isInitialized = true;
                 if (result) {
                     this.participantState = JSON.parse(result);
-                    if(this.participantState){
-                            this.userName = this.label.PPWELCOME +', ' +this.participantState.loggedInUserName;
+                    if (this.participantState) {
+                        let username = this.currentMode.groupLabel;
+                        let firstName = username.substring(0, username.indexOf(' '));
+
+                        this.userName = this.label.PPWELCOME + ', ' + firstName;
                     }
                     if (this.participantState.pe) {
                         if (this.participantState.pe.Clinical_Trial_Profile__r) {
@@ -37,13 +65,23 @@ export default class HomePageParticipantNew extends LightningElement {
                         }
                     }
                     //For Delegate Self view
-                    this.isDelegateSelfview = (this.participantState.value == 'ALUMNI') && this.participantState.hasPatientDelegates && !this.participantState.isDelegate;
-
+                    this.isDelegateSelfview =
+                        this.participantState.value == 'ALUMNI' &&
+                        this.participantState.hasPatientDelegates &&
+                        !this.participantState.isDelegate;
                 }
+                this.spinner.hide();
             })
             .catch((error) => {
                 console.log('error::' + JSON.stringify(error));
                 this.error = error;
+                this.spinner.hide();
             });
+    }
+
+    renderedCallback() {
+        if (this.isInitialized == true) {
+            this.spinner = this.template.querySelector('c-web-spinner');
+        }
     }
 }
