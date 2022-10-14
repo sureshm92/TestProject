f29b80746249ef2ac4314e0e2f2a44b3a8d7c756
u1138012 <rahul.kumar3@iqvia.com>
2022-10-14 18:51:49 +0530

    Extra line removed

--- force-app/main/community-template-pp_2/common/lwc/ppDateTimeCombo/ppDateTimeCombo.js
+++ force-app/main/community-template-pp_2/common/lwc/ppDateTimeCombo/ppDateTimeCombo.js
@@ -200,7 +200,6 @@ export default class PpDateTimeCombo extends LightningElement {
     handleTime(event) {
         this.initialTimeLoaded = true;
         this.tm = event.target.value;
-
         if (!this.tm) {
             this.timeOnlyPresent = false;
             const timeOnly = new CustomEvent('time', {
