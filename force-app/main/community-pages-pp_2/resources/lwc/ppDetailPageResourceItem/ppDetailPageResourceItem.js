af6f698ce346e6ddb209f759ae560230fb0075a0
u1138012 <rahul.kumar3@iqvia.com>
2022-11-14 19:00:19 +0530

    PEH 7603, 8397 UAT deployment

---
+++ force-app/main/community-pages-pp_2/resources/lwc/ppDetailPageResourceItem/ppDetailPageResourceItem.js
@@ -0,0 +1,32 @@
+import { LightningElement, api } from 'lwc';
+
+export default class PpDetailPageResourceItem extends LightningElement {
+    @api resourceType;
+    @api resourceLink;
+    showArticle = false;
+    showVideo = false;
+    isThumbnailPresent = false;
+
+    connectedCallback() {
+        this.spinner = this.template.querySelector('c-web-spinner');
+        if (this.spinner) {
+            this.spinner.show();
+        }
+        this.isThumbnailPresent = this.resourceLink ? true : false;
+        if (this.spinner) {
+            this.spinner.show();
+        }
+        if (this.resourceType == 'Article') {
+            this.showArticle = true;
+        }
+        if (this.resourceType == 'Video') {
+            this.showVideo = true;
+        }
+        if (this.spinner) {
+            this.spinner.hide();
+        }
+    }
+    handleError() {
+        this.isThumbnailPresent = false;
+    }
+}
