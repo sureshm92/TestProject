8b9976750f6b8749567fd4bd273a14afed5b4221
u1138544 <chetna.chauhan@iqvia.com>
2022-11-23 13:21:01 +0530

    PEH-7609/7610 backend changes

--- force-app/main/community-pages-pp_2/updates/lwc/ppUpdates/ppUpdates.js
+++ force-app/main/community-pages-pp_2/updates/lwc/ppUpdates/ppUpdates.js
@@ -1,8 +1,4 @@
 import { LightningElement, track } from 'lwc';
-import RR_COMMUNITY_JS from '@salesforce/resourceUrl/rr_community_js';
-import { loadScript, loadStyle } from 'lightning/platformResourceLoader';
-import communityPPTheme from '@salesforce/resourceUrl/Community_CSS_PP_Theme';
-
 import getInitDataNew from '@salesforce/apex/RelevantLinksRemote.getInitDataNew';
 import getUpdateResources from '@salesforce/apex/ResourceRemote.getUpdateResources';
 import pp_community_icons from '@salesforce/resourceUrl/pp_community_icons';
@@ -27,23 +23,9 @@ export default class PpUpdates extends LightningElement {
     connectedCallback() {
         DEVICE != 'Small' ? (this.desktop = true) : (this.desktop = false);
 
-        loadScript(this, RR_COMMUNITY_JS)
-            .then(() => {
-                Promise.all([loadStyle(this, communityPPTheme)])
-                    .then(() => {
-                        this.spinner = this.template.querySelector('c-web-spinner');
-                        this.spinner ? this.spinner.show() : '';
-                        // Getting data for Updates section. For now only Links data is fetched
-                        // Need backend work to filter data as per AC on rel links, articles, videos, documents
-                        this.initializeData();
-                    })
-                    .catch((error) => {
-                        console.log(error.body.message);
-                    });
-            })
-            .catch((error) => {
-                communityService.showToast('', 'error', error.message, 100);
-            });
+        this.spinner = this.template.querySelector('c-web-spinner');
+        this.spinner ? this.spinner.show() : '';
+        this.initializeData();
     }
 
     initializeData() {
@@ -83,10 +65,7 @@ export default class PpUpdates extends LightningElement {
                     this.updateData.push(resObj.resource);
                 });
                 this.updateData.forEach((resObj) => {
-                    if (
-                        resObj.Content_Type__c == 'Article' ||
-                        resObj.Content_Type__c == 'Video'
-                    ) {
+                    if (resObj.Content_Type__c == 'Article' || resObj.Content_Type__c == 'Video') {
                         resObj.isExplore = true;
                     } else if (resObj.Content_Type__c == 'Study_Document') {
                         resObj.isDoc = true;
