995807aa8b7d0423290c95e530dd1ab09cee5e7e
naresh <naresh.babu2@iqvia.com>
2022-11-23 12:10:45 +0530

    Merge branch 'INT14' of https://gitlabrnds.quintiles.com/e2e-pe/rhpp-project into pp12.0/pp-revamp/newu1120070

--- force-app/main/community-pages-pp/task-detail/aura/StudyVisitReminder/StudyVisitReminderController.js
+++ force-app/main/community-pages-pp/task-detail/aura/StudyVisitReminder/StudyVisitReminderController.js
@@ -78,7 +78,7 @@
         var smsOptIn = component.get('v.smsOptIn');
         var reminderOption = component.get('v.task.Remind_Me__c');
         var deleteVisitTask = component.get('v.deleteVisitReminder');
-
+        var clearTask = component.get('v.task.clearReminderValue');
         if (new Date(dueDateOrplanDate) < new Date()) {
             communityService.showErrorToast('', $A.get('$Label.c.PP_ReminderUnderFlowError'), 3000);
             return;
@@ -87,7 +87,8 @@
             !$A.util.isUndefinedOrNull(reminderOption) &&
             !(smsOptIn && smsPeferenceSelected) &&
             !(emailOptIn && emailPeferenceSelected) &&
-            !deleteVisitTask
+            !deleteVisitTask &&
+            !clearTask
         ) {
             communityService.showErrorToast('', $A.get('$Label.c.PP_Remind_Using_Required'), 3000);
             return;
@@ -155,7 +156,8 @@
                 !$A.util.isUndefinedOrNull(reminderOption)) &&
             !(smsPeferenceSelected && smsOptIn) &&
             !(emailPeferenceSelected && emailOptIn) &&
-            !deleteVisitTask
+            !deleteVisitTask &&
+            !clearTask
         ) {
             communityService.showErrorToast('', $A.get('$Label.c.PP_Remind_Using_Required'), 3000);
             return;
@@ -243,7 +245,7 @@
         component.set('v.initData.today', new Date(new Date() + 60 * 1000));
 
         if (remindMe !== 'Custom') {
-            if (remindMe === '1 Week before') {
+            if (remindMe === '1 week before') {
                 isGreaterThanToday = moment(dueDateOrplanDate).subtract(7, 'days').isBefore(today);
             } else if (remindMe === '1 day before') {
                 isGreaterThanToday = moment(dueDateOrplanDate).subtract(1, 'days').isBefore(today);
@@ -254,11 +256,18 @@
                 var reminderdate = new Date(dueDateOrplanDate) - 4 * 3600 * 1000;
                 isGreaterThanToday = new Date() > new Date(reminderdate);
             }
-            if (remindMe === 'No reminder') {
+            if (remindMe === 'No reminder' && task.Task_Type__c === 'Visit') {
                 component.set('v.initData.reminderDate', null);
                 component.set('v.deleteVisitReminder', true);
                 component.set('v.task.Remind_Using_Email__c', false);
                 component.set('v.task.Remind_Using_SMS__c', false);
+                component.set('v.task.clearReminderValue', true);
+            } else if (remindMe === 'No reminder') {
+                component.set('v.initData.reminderDate', null);
+                component.set('v.deleteVisitReminder', false);
+                component.set('v.task.Remind_Using_Email__c', false);
+                component.set('v.task.Remind_Using_SMS__c', false);
+                component.set('v.task.clearReminderValue', true);
             }
             if (isGreaterThanToday) {
                 if (!$A.util.isUndefinedOrNull(reminderOptionValid)) {
