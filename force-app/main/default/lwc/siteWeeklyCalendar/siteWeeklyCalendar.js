d09c043ffec4f59599682f27bb01ed7184076546
velu <veluman.vijayaragavan@quitiles.com>
2022-12-22 14:33:49 +0530

    RH - 9585

--- force-app/main/default/lwc/siteWeeklyCalendar/siteWeeklyCalendar.js
+++ force-app/main/default/lwc/siteWeeklyCalendar/siteWeeklyCalendar.js
@@ -13,6 +13,9 @@ import September from '@salesforce/label/c.September';
 import October from '@salesforce/label/c.October';
 import November from '@salesforce/label/c.November';
 import December from '@salesforce/label/c.December';
+import MaxWeek from '@salesforce/label/c.RH_MaxNoWeek';
+import MinWeek from '@salesforce/label/c.RH_MinNoWeek';
+import TodayLabel from '@salesforce/label/c.DRF_L_Today';
 
 const month = [
   January,
@@ -35,13 +38,22 @@ const todayDate = new Date();
 export default class SiteWeeklyCalendar extends LightningElement {
   @api dayList = [];
   @api currentDay;
-  @api isNext = false;
-  @api isPrev = false;
+  isNext = false;
+  isPrev = false;
   @api currentMonth;
   @api currentYear;
   currentDate;
   selectedDay;
+  selectedMonth;
   selectedDate;
+  selectedNextWeek = 0;
+  selectedPrevWeek = 0;
+  label = {
+    MaxWeek,
+    MinWeek,
+    TodayLabel
+  };
+  isCurrentWeek;
 
   connectedCallback() {
     let userDate = new Date(
@@ -56,6 +68,7 @@ export default class SiteWeeklyCalendar extends LightningElement {
     this.currentMonth = month[userDate.getMonth()];
     this.currentYear = userDate.getFullYear();
     let dayn = 0;
+    this.isCurrentWeek = true;
 
     for (let i = 0; i < 7; i++) {
       let calData = {};
@@ -75,10 +88,10 @@ export default class SiteWeeklyCalendar extends LightningElement {
     }
 
     if (this.selectedDay) {
-      this.userSelectedDay(this.selectedDay);
-      let selectedmonth = month[this.selectedDate.getMonth()];
+      this.userSelectedDay(this.selectedDate);
+      this.selectedmonth = month[this.selectedDate.getMonth()];
       let selectedYear = this.selectedDate.getFullYear();
-      this.currentMonth = selectedmonth;
+      this.currentMonth = this.selectedmonth;
       this.currentYear = selectedYear;
     }
   }
@@ -93,113 +106,189 @@ export default class SiteWeeklyCalendar extends LightningElement {
     return userDate;
   }
 
-  handleNext() {
-    this.isNext = true;
-    this.dayList = [];
-    if (this.isPrev) {
-      this.connectedCallback();
-    } else {
-      this.isPrev = false;
-      var dayn = 0;
+  handlePastFutureDates(event) {
+    let selectedEvent = event.currentTarget.dataset.whichevent;
 
-      for (let i = 0; i < 7; i++) {
-        let css = "";
-        let selectedCss = "slds-is-today";
-        let calData = {};
-        let dateValue = null;
-        if (i <= this.currentDay) {
-          dateValue = this.fetchDay(
-            this.currentDate + (7 - (this.currentDay - i))
-          );
-          dayn = dateValue.getDate();
-        } else if (i > this.currentDay) {
-          dateValue = this.fetchDay(
-            this.currentDate + (7 + (i - this.currentDay))
-          );
-          dayn = dateValue.getDate();
-        }
-        if (dayn == this.selectedDay) {
-          calData = { dayn: dayn, css: selectedCss, datevalue: dateValue };
-        } else {
-          calData = { dayn: dayn, css: css, datevalue: dateValue };
-        }
-        this.dayList.push(calData);
-      }
-      if (this.selectedDay) {
-        let selectedmonth = month[this.selectedDate.getMonth()];
-        let selectedYear = this.selectedDate.getFullYear();
-        this.currentMonth = selectedmonth;
-        this.currentYear = selectedYear;
-      }
-    }
-  }
-  handlePrev() {
-    this.dayList = [];
-    this.isPrev = true;
-    if (this.isNext) {
+    if(selectedEvent === 'next' && !this.isNext) {
+      this.selectedNextWeek++;
+    } else if(selectedEvent === 'prev' && !this.isPrev) {
+      this.selectedPrevWeek++;
+    }   
+    let selectedWeek = this.selectedNextWeek - this.selectedPrevWeek;
+    
+    
+    if( selectedWeek > 0) {
+      this.handleNext(Math.abs(selectedWeek));
+    } else if(selectedWeek < 0) {
+      this.handlePrev(Math.abs(selectedWeek));
+    } else {
       this.connectedCallback();
+    }
+    if(selectedWeek == parseInt(this.label.MaxWeek)) {
+      this.isNext = true;
+      this.template.querySelector(".next").classList.add("disable-next");
     } else {
       this.isNext = false;
-      let dayn = 0;
-      for (let i = 0; i < 7; i++) {
-        let css = "";
-        let selectedCss = "slds-is-today";
-        let calData = {};
-        let dateValue = null;
-        if (i <= this.currentDay) {
-          dateValue = this.fetchDay(
-            this.currentDate - (7 + this.currentDay - i)
-          );
-          dayn = dateValue.getDate();
-        } else if (i > this.currentDay) {
-          dateValue = this.fetchDay(
-            this.currentDate - (7 + this.currentDay - i)
-          );
-          dayn = dateValue.getDate();
-        }
-        if (dayn == this.selectedDay) {
-          calData = { dayn: dayn, css: selectedCss, datevalue: dateValue };
-        } else {
-          calData = { dayn: dayn, css: css, datevalue: dateValue };
-        }
-        this.dayList.push(calData);
+      if(this.template.querySelector(".disable-next")) {
+        this.template.querySelector(".next").classList.remove("disable-next");
       }
-      if (this.selectedDay) {
-        let selectedmonth = month[this.selectedDate.getMonth()];
-        let selectedYear = this.selectedDate.getFullYear();
-        this.currentMonth = selectedmonth;
-        this.currentYear = selectedYear;
+    }
+    if(selectedWeek == parseInt(-this.label.MinWeek)) {
+      this.isPrev = true;
+      this.template.querySelector(".prev").classList.add("disable-prev");
+    } else {
+      this.isPrev = false;
+      if(this.template.querySelector(".disable-prev")) {
+        this.template.querySelector(".prev").classList.remove("disable-prev");
       }
+    }
+    
+  }
+
+  handleNext(currentWeek) {
+    this.dayList = [];
+    var dayn = 0;
+    let daym = 0;
+    this.isCurrentWeek = false;
+    for (let i = 0; i < 7; i++) {
+      let css = "";
+      let selectedCss = "slds-is-today";
+      let calData = {};
+      let dateValue = null;
+      if (i <= this.currentDay) {
+        dateValue = this.fetchDay(
+          this.currentDate + (7+((currentWeek-1) * 7) - (this.currentDay - i))
+        );
+        dayn = dateValue.getDate();
+        daym = month[dateValue.getMonth()];
+      } else if (i > this.currentDay) {
+        dateValue = this.fetchDay(
+          this.currentDate + (7+((currentWeek-1) * 7) + (i - this.currentDay))
+        );
+        dayn = dateValue.getDate();
+        daym =month[dateValue.getMonth()];
+      }
+      if (dayn == this.selectedDay && daym == this.selectedmonth) {
+        calData = { dayn: dayn, css: selectedCss, datevalue: dateValue };
+      } else {
+        calData = { dayn: dayn, css: css, datevalue: dateValue };
+      }
+      this.dayList.push(calData);
+      
+    }
+    if (this.selectedDay) {
+      this.selectedmonth = month[this.selectedDate.getMonth()];
+      let selectedYear = this.selectedDate.getFullYear();
+      this.currentMonth = this.selectedmonth;
+      this.currentYear = selectedYear;
+    }
+  }
+
+  handlePrev(currentWeek) {
+    this.dayList = [];
+    let dayn = 0;
+    let daym = 0;
+    this.isCurrentWeek = false;
+    for (let i = 0; i < 7; i++) {
+      let css = "";
+      let selectedCss = "slds-is-today";
+      let calData = {};
+      let dateValue = null;
+      if (i <= this.currentDay) {
+       
+        dateValue = this.fetchDay(
+          this.currentDate - (7+((currentWeek-1) * 7) + this.currentDay - i)
+        );
+        dayn = dateValue.getDate();
+        daym = month[dateValue.getMonth()];
+      } else if (i > this.currentDay) {
+        dateValue = this.fetchDay(
+          this.currentDate - (7+((currentWeek-1) * 7) + this.currentDay - i)
+        );
+        dayn = dateValue.getDate();
+        daym = month[dateValue.getMonth()];
+      }
+      if (dayn == this.selectedDay && daym == this.selectedmonth) {
+        calData = { dayn: dayn, css: selectedCss, datevalue: dateValue };
+      } else {
+        calData = { dayn: dayn, css: css, datevalue: dateValue };
+      }
+      this.dayList.push(calData);
+    }
+    if (this.selectedDay) {
+      this.selectedmonth = month[this.selectedDate.getMonth()];
+      let selectedYear = this.selectedDate.getFullYear();
+      this.currentMonth = this.selectedmonth;
+      this.currentYear = selectedYear;
     }
   }
 
   selectDate(event) {
     let selectedDate = new Date(event.currentTarget.dataset.whichdate);
-    this.selectedDate = selectedDate;
-    this.selectedDay = selectedDate.getDate();
-    let dayListTemp = this.userSelectedDay(this.selectedDay);
+    this.fetchDataOnDateSelected(selectedDate);
+  }
+
+  fetchDataOnDateSelected(selecteddateval) {
+    this.selectedDate = selecteddateval;
+    let dayListTemp = this.userSelectedDay(this.selectedDate);
     this.dayList = dayListTemp;
-    let selectedmonth = month[selectedDate.getMonth()];
-    let numMOnth = month.indexOf(selectedmonth) + 1;
-    let selectedYear = selectedDate.getFullYear();
-    this.currentMonth = selectedmonth;
+    this.selectedmonth = month[this.selectedDate.getMonth()];
+    let numMOnth = month.indexOf(this.selectedmonth) + 1;
+    let selectedYear = this.selectedDate.getFullYear();
+    this.currentMonth = this.selectedmonth;
     this.currentYear = selectedYear;
     let fullDate = `${this.currentYear}-${numMOnth}-${this.selectedDay}`;
     const selectedEvent = new CustomEvent("selected", { detail: fullDate });
 
     // Dispatches the event.
     this.dispatchEvent(selectedEvent);
-  }
 
-  userSelectedDay(day) {
+  }
+  userSelectedDay(selectedDate) {
+    this.selectedDay = selectedDate.getDate();
+    this.selectedmonth = month[selectedDate.getMonth()];
     let dayListTemp = this.dayList;
     for (let i = 0; i < dayListTemp.length; i++) {
-      if (dayListTemp[i].dayn == day) {
+      //let iDate = new Date(dayListTemp[i].datevalue);
+      let monthval = new Date(dayListTemp[i].datevalue).getMonth();
+      let monthText = month[monthval];
+      if (dayListTemp[i].dayn == this.selectedDay && monthText == this.selectedmonth) {
         dayListTemp[i].css = "slds-is-today";
       } else {
         dayListTemp[i].css = "";
       }
     }
     return dayListTemp;
+  }
+
+  handleToday() {
+    this.selectedNextWeek = 0;
+    this.selectedPrevWeek = 0;
+    let userDate = new Date(
+      todayDate.toLocaleString(locale, { timeZone: timeZone })
+    );    
+    this.selectedDate = userDate;
+    this.selectedDay = userDate.getDate();
+    this.fetchDataOnDateSelected(this.selectedDate);
+    let selectedWeek = this.selectedNextWeek - this.selectedPrevWeek;
+    if(selectedWeek == parseInt(this.label.MaxWeek)) {
+      this.isNext = true;
+      this.template.querySelector(".next").classList.add("disable-next");
+    } else {
+      this.isNext = false;
+      if(this.template.querySelector(".disable-next")) {
+        this.template.querySelector(".next").classList.remove("disable-next");
+      }
+    }
+    if(selectedWeek == parseInt(-this.label.MinWeek)) {
+      this.isPrev = true;
+      this.template.querySelector(".prev").classList.add("disable-prev");
+    } else {
+      this.isPrev = false;
+      if(this.template.querySelector(".disable-prev")) {
+        this.template.querySelector(".prev").classList.remove("disable-prev");
+      }
+    }
+    this.connectedCallback();
   }
 }
