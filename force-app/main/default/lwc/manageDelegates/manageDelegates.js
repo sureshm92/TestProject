8c22a734873b1de9ccb2f5155e795922af5a5cdb
q1058169 <krishnakumar.mahto@quintiles.com>
2023-01-13 13:15:50 +0530

    Revert variable removal.

--- force-app/main/default/lwc/manageDelegates/manageDelegates.js
+++ force-app/main/default/lwc/manageDelegates/manageDelegates.js
@@ -42,6 +42,8 @@ import {
 export default class ManageDelegates extends NavigationMixin(LightningElement) {
     isDesktop;
     @api participantState;
+    @api isDelegate;
+    @api consentPreferenceData;
     @api userMode;
     @api isRTL;
     @track listPDE = [];
