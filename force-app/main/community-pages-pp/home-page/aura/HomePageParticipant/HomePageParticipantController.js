c1a61bd016af4053e2eb89bdeea5de1a200c6690
chavva Anitha <chavva.anitha@iqvia.com>
2023-01-27 13:08:45 +0530

    Ps int14-1

--- force-app/main/community-pages-pp/home-page/aura/HomePageParticipant/HomePageParticipantController.js
+++ force-app/main/community-pages-pp/home-page/aura/HomePageParticipant/HomePageParticipantController.js
@@ -3,36 +3,41 @@
  */
 ({
     doInit: function (component, event, helper) {
-        if (communityService.getCurrentCommunityMode().hasPastStudies) {
-            component.set(
-                'v.showPastStudies',
-                communityService.getCurrentCommunityMode().hasPastStudies
-            );
-        }
-
-        communityService.executeAction(component, 'getInitData', null, function (returnValue) {
-            var ps = JSON.parse(returnValue);
-            component.set('v.participantState', ps);
-
-            component.set(
-                'v.isDelegateMode',
-                communityService.getCurrentCommunityMode().currentDelegateId
-            );
-
-            if (
-                ps.communityName === 'IQVIA Referral Hub' ||
-                ps.communityName === 'IQVIA Patient Portal'
-            )
+        if (communityService.isInitialized()) {
+            component.set('v.communityService', communityService);
+            if (communityService.getCurrentCommunityMode().hasPastStudies) {
                 component.set(
-                    'v.showTrialSearch',
-                    !communityService.getCurrentCommunityMode().currentDelegateId &&
-                        !ps.participant.Marketing_Flag__c &&
-                        !ps.pe
+                    'v.showPastStudies',
+                    communityService.getCurrentCommunityMode().hasPastStudies
                 );
-
-            component.find('spinner').hide();
-            component.set('v.initialized', true);
-        });
+            }
+            communityService.executeAction(component, 'getInitData', null, function (returnValue) {
+                var ps = JSON.parse(returnValue);
+                component.set('v.participantState', ps);
+    
+                component.set(
+                    'v.isDelegateMode',
+                    communityService.getCurrentCommunityMode().currentDelegateId
+                );
+    
+                if (
+                    ps.communityName === 'IQVIA Referral Hub' ||
+                    ps.communityName === 'IQVIA Patient Portal'
+                )
+                    component.set(
+                        'v.showTrialSearch',
+                        !communityService.getCurrentCommunityMode().currentDelegateId &&
+                            !ps.participant.Marketing_Flag__c &&
+                            !ps.pe
+                    );
+    
+                component.find('spinner').hide();
+                component.set('v.initialized', true);
+            });    
+        }
+        else {
+            communityService.initialize(component);
+        }
     },
 
     navigateToTrialSearchPage: function (component, event, helper) {
