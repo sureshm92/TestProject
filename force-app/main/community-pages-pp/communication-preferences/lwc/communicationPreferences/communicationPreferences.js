c16143e3bed4187eb7785a3dd71d29df9a516b68
melishadsa <melisha.dsa@iqvia.com>
2023-01-31 17:30:42 +0530

    PEH-10748  All your consent replace with this consent

--- force-app/main/community-pages-pp/communication-preferences/lwc/communicationPreferences/communicationPreferences.js
+++ force-app/main/community-pages-pp/communication-preferences/lwc/communicationPreferences/communicationPreferences.js
@@ -34,7 +34,6 @@ import BTN_Save from '@salesforce/label/c.BTN_Save';
 
 import PP_Study_Consent_Adult_ROW from '@salesforce/label/c.PP_Study_Consent_Adult_ROW';
 import PP_Study_Consent_Adult_US from '@salesforce/label/c.PP_Study_Consent_Adult_US';
-import PP_Communication_Purchase_Pref from '@salesforce/label/c.PP_Communication_Purchase_Pref';
 import PP_Communication_Purchase_Pref_V2 from '@salesforce/label/c.PP_Communication_Purchase_Pref_V2';
 import PP_IQVIA_Communication_US from '@salesforce/label/c.PP_IQVIA_Communication_US';
 import PP_Outreach_Communication_Pref_A from '@salesforce/label/c.PP_Outreach_Communication_Pref_A';
@@ -142,8 +141,6 @@ export default class CommunicationPreferences extends NavigationMixin(LightningE
     errorIconPosition = 'margin-left: 0px';
 
     isCountryUS = false;
-    showDelPurchaseStudy = false;
-    showDelPurchaseIqvia = false;
     //@track phoneSvg = rr_community_icons +'/'+'logo.svg';
     phoneSvg = rr_community_icons + '/' + 'com-phone.svg' + '#' + 'com-phone';
     emailSvg = rr_community_icons + '/' + 'com-email.svg' + '#' + 'com-email';
@@ -191,8 +188,6 @@ export default class CommunicationPreferences extends NavigationMixin(LightningE
                 this.isCountryUS = (this.consentPreferenceDataLocal.myContact.MailingCountry!= undefined &&  this.consentPreferenceDataLocal.myContact.MailingCountry == 'United States' ? true : false);
                 let programOverViewData = JSON.parse(result).programOverViewData;
                 let participantState = JSON.parse(result).participantState;
-                this.showDelPurchaseIqvia = (programOverViewData.isDelegateSelfView || participantState.isDelegate || programOverViewData.isAlumniParticipant);
-                this.showDelPurchaseStudy = (programOverViewData.isDelegateSelfView || (participantState.isDelegate && (this.consentPreferenceDataLocal.isPrimaryDelegate == false)) || programOverViewData.isAlumniParticipant );
                 if (!this.isMobilePhoneNumberAvailable) {
                     this.studyError = this.checkSMSCheckedOrNot();
                 }
