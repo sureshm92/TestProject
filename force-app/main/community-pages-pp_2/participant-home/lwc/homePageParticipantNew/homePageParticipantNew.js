a43c1414e4e55afe253f0ce4f5971c89050245ad
raviteja.paidikalva <raviteja.paidikalva@iqvia.com>
2023-03-24 17:49:22 +0530

    Delegate Home Page Changes

--- force-app/main/community-pages-pp_2/participant-home/lwc/homePageParticipantNew/homePageParticipantNew.js
+++ force-app/main/community-pages-pp_2/participant-home/lwc/homePageParticipantNew/homePageParticipantNew.js
@@ -135,6 +135,10 @@ export default class HomePageParticipantNew extends LightningElement {
                             }
 
                             
+                    }else if(this.participantState.value == 'ALUMNI' ||
+                            (this.participantState.hasPatientDelegates && !this.participantState.isDelegate)){
+                        this.showTelevisitCard = true;
+                        this.isTelevisits = true;
                     }
                     if (this.desktop != true) {
                         //this.updatesSection = true;
