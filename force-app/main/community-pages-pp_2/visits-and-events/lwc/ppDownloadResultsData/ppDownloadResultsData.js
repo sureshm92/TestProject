6f8cb247d4fc108b509b19b660f73fdd994ab383
Jayashree J <jayashree.j@iqvia.com>
2023-07-28 16:02:51 +0530

    .

--- force-app/main/community-pages-pp_2/visits-and-events/lwc/ppDownloadResultsData/ppDownloadResultsData.js
+++ force-app/main/community-pages-pp_2/visits-and-events/lwc/ppDownloadResultsData/ppDownloadResultsData.js
@@ -1,10 +1,5 @@
 import { LightningElement, api } from 'lwc';
 import PP_Download_Results_Data from '@salesforce/label/c.PP_Download_Results_Data';
-import Download_PP from '@salesforce/label/c.Download_PP';
-import Open_In_New_Tab_PP from '@salesforce/label/c.Open_In_New_Tab_PP';
-import Download_In_Progress_PP from '@salesforce/label/c.Download_In_Progress_PP';
-import Study_Data_Report from '@salesforce/label/c.Study_Data_Report';
-import Cumulative_Study_Data_Report from '@salesforce/label/c.Cumulative_Study_Data_Report';
 import getBase64fromVisitSummaryReportPage_Modified from '@salesforce/apex/ModifiedVisitReportContainerRemote.getBase64fromVisitSummaryReportPage_Modified';
 import FORM_FACTOR from '@salesforce/client/formFactor';
 import mobileTemplate from './ppDownloadResultsDataMobile.html';
@@ -12,6 +7,7 @@ import tabletTemplate from './ppDownloadResultsDataTablet.html';
 import desktopTemplate from './ppDownloadResultsData.html';
 import rtlLanguages from '@salesforce/label/c.RTL_Languages';
 import checkifPrimary from '@salesforce/apex/ppFileUploadController.checkifPrimary';
+
 export default class PpDownloadResultsData extends LightningElement {
     peId;
     isRTL;
@@ -21,16 +17,9 @@ export default class PpDownloadResultsData extends LightningElement {
     @api patientVisitId;
     showDownloadResults;
     label = {
-        PP_Download_Results_Data,
-        Open_In_New_Tab_PP,
-        Download_PP,
-        Download_In_Progress_PP,
-        Study_Data_Report,
-        Cumulative_Study_Data_Report
+        PP_Download_Results_Data
     };
-    val = false;
-    onFirstLoad = false;
-    viewResults;
+
     connectedCallback() {
         this.userDetails = communityService.getLoggedInUserData();
         if (this.alumniPeId != null) {
@@ -44,9 +33,7 @@ export default class PpDownloadResultsData extends LightningElement {
         } else {
             this.showDownloadResults = true;
         }
-        //    this.onFirstLoad = true;
     }
-
     renderedCallback() {
         if (this.alumniPeId != null) {
             this.peId = this.alumniPeId;
@@ -101,9 +88,7 @@ export default class PpDownloadResultsData extends LightningElement {
     get isTablet() {
         return FORM_FACTOR == 'Medium';
     }
-
-    viewReport() {
-        console.log('JJ');
+    generateReport() {
         if (!this.isDesktop) {
             getBase64fromVisitSummaryReportPage_Modified({
                 peId: this.peId,
@@ -120,80 +105,20 @@ export default class PpDownloadResultsData extends LightningElement {
         }
 
         if (this.patientVisitId != null) {
-            let url =
+            window.open(
                 '/pp/apex/PatientVisitReportPage?peId=' +
-                this.peId +
-                '&isRTL=' +
-                this.isRTL +
-                '&patientVisitNam=' +
-                this.patientVisitNam +
-                '&patientVisitId=' +
-                this.patientVisitId;
-            window.open(url);
+                    this.peId +
+                    '&isRTL=' +
+                    this.isRTL +
+                    '&patientVisitNam=' +
+                    this.patientVisitNam +
+                    '&patientVisitId=' +
+                    this.patientVisitId
+            );
         } else {
-            let url =
-                '/pp/apex/PatientVisitReportPage?peId=' +
-                this.peId +
-                '&isRTL=' +
-                this.isRTL +
-                '&patientVisitNam=' +
-                this.patientVisitNam +
-                '&patientVisitId=' +
-                this.patientVisitId;
-            window.open(url);
+            window.open(
+                '/pp/apex/PatientVisitReportPage?peId=' + this.alumniPeId + '&isRTL=' + this.isRTL
+            );
         }
-    }
-    downloadReport(event) {
-        this.template
-            .querySelector('c-custom-toast-files-p-p')
-            .showToast('success', 'Download_In_Progress_PP', 'utility:download', 10000);
-        let anchorEle = '';
-        anchorEle = this.template.querySelectorAll('.download-results');
-        // if (!this.isDesktop) {
-        //     getBase64fromVisitSummaryReportPage_Modified({
-        //         peId: this.peId,
-        //         isRTL: this.isRTL,
-        //         patientVisitNam: this.patientVisitNam,
-        //         patientVisId: this.patientVisitId
-        //     })
-        //         .then((returnValue) => {
-        //             communityService.navigateToPage('mobile-pdf-viewer?pdfData=' + returnValue);
-        //         })
-        //         .catch((error) => {
-        //             console.error('Error occured during report generation', error.message, 'error');
-        //         });
-        // }
-
-        if (this.patientVisitId != null) {
-            let url =
-                '/pp/apex/PatientVisitReportPage?peId=' +
-                this.peId +
-                '&isRTL=' +
-                this.isRTL +
-                '&patientVisitNam=' +
-                this.patientVisitNam +
-                '&patientVisitId=' +
-                this.patientVisitId;
-            anchorEle[0].setAttribute('href', url);
-            if (this.onFirstLoad) {
-                this.onFirstLoad = false;
-                anchorEle[0].click();
-            } else {
-                this.onFirstLoad = true;
-            }
-        } else {
-            let url =
-                '/pp/apex/PatientVisitReportPage?peId=' + this.alumniPeId + '&isRTL=' + this.isRTL;
-            anchorEle[0].setAttribute('href', url);
-            //    anchorEle[0].click();
-        }
-    }
-    toggleElement() {
-        let ddMenu = this.template.querySelector('[data-id="dropdown-menu"]');
-        ddMenu.classList.toggle('active');
-    }
-    removeElementFocus() {
-        let ddMenu = this.template.querySelector('[data-id="dropdown-menu"]');
-        ddMenu.classList.remove('active');
     }
 }
